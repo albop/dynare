7a79e4dd2f6be0c650d0b19cacedd34d125bdbf6
michel <michel@ac1d8469-bf42-47a9-8791-bf33cf982152>
2005-05-02 11:04:37 +0000

    removed useless tests added info
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v3@175 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/resol.m
+++ matlab/resol.m
@@ -1,41 +1,18 @@
 % Copyright (C) 2001 Michel Juillard
 %
-function [dr,info]=resol(ys,algo,linear,iorder)
+function [dr,info]=resol(ys,check_flag)
 
 global jacobia_ iy_ ykmin_ ykmax_ gstep_ exo_nbr exo_det_nbr endo_nbr
 global ex_ ex_det_ valf_ it_ exe_ exe_det_ xkmin_ xkmax_ 
 global fname_ means_ stderrs_ lgy_ maxit_
 global dynatol_ options_
-global BlanchardKahn_
 
+info = 0;
 
-if linear == 1
-  iorder =1;
-end
-
-xlen = xkmax_ + xkmin_ + 1;
-klen = ykmin_ + ykmax_ + 1;
-iyv = iy_';
-iyv = iyv(:);
-iyr0 = find(iyv) ;
 it_ = ykmin_ + 1 ;
 
 if exo_nbr == 0
   exe_ = [] ;
-end
-
-if ~ iy_(ykmin_+1,:) > 0
-  error ('RESOL: Error in model specification: some variables don"t appear as current') ;
-end
-
-if xlen > 1
-  error (['RESOL: stochastic exogenous variables must appear only at the' ...
-	  ' current period. Use additional endogenous variables']) ;
-end
-
-if (exo_det_nbr > 0) & (ykmin_ > 1 | ykmax_ > 1)
-  error(['Exogenous deterministic variables are currently only allowed in' ...
-	 ' models with leads and lags on only one period'])
 end
 
 % check if ys is steady state
@@ -48,24 +25,35 @@ end
 fh = str2func([fname_ '_fff']);
 if max(abs(feval(fh,ys))) > dynatol_
   if exist([fname_ '_steadystate'])
-    [dr.ys,check] = feval([fname_ '_steadystate'],ys);
+    [dr.ys,check1] = feval([fname_ '_steadystate'],ys);
   else
-    [dr.ys,check] = dynare_solve([fname_ '_fff'],ys);
+    [dr.ys,check1] = dynare_solve([fname_ '_fff'],ys);
   end
-  if check
-    error('RESOL: convergence problem in DYNARE_SOLVE')
+  if check1
+    info(1) = 20;
+    resid = feval(fh,ys);
+    info(2) = resid'*resid; % penalty...
+    return
   end
 else
   dr.ys = ys;
 end
 
 dr.fbias = zeros(endo_nbr,1);
-[dr,info] = dr1(iorder,dr,0);
+[dr,info] = dr1(dr,check_flag);
 
-if algo == 1 & iorder > 1
+if info(1)
+  return
+end
+
+if options_.dr_algo == 1 & options_.order > 1
   dr.ys = dynare_solve('dr2',ys,dr);
   dr.fbias = 2*feval([fname_ '_fff'],dr.ys);
-  dr = dr1(iorder,dr,0);
+  [dr, info1] = dr1(dr,check_flag);
+  if info1(1)
+    info(1) = info(1)+10;
+    return
+  end
 end
 ex_det_ = tempexdet;
 ex_ = tempex;
