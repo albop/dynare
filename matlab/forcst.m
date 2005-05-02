39e66f63b0f186864031d7c32366b7d9f0f68ee0
michel <michel@ac1d8469-bf42-47a9-8791-bf33cf982152>
2005-05-02 15:39:53 +0000

    added output
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v3@193 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/forcst.m
+++ matlab/forcst.m
@@ -1,5 +1,5 @@
-function [yf,var_yf]=forcst(dr,y0,k,m)
-  global endo_nbr exo_nbr ykmin_ Sigma_e_ ex_ options_
+function [yf,var_yf]=forcst(dr,y0,k,var_list)
+  global endo_nbr exo_nbr ykmin_ Sigma_e_ ex_ options_ lgy_
   
   options_.periods = k;
   make_ex_;
@@ -59,8 +59,33 @@ function [yf,var_yf]=forcst(dr,y0,k,m)
     end
   end
   
+  nvar = size(var_list,1);
+  if nvar == 0
+    nvar = endo_nbr;
+    ivar = [1:nvar];
+  else
+    ivar=zeros(nvar,1);
+    for i=1:nvar
+      i_tmp = strmatch(var_list(i,:),lgy_,'exact');
+      if isempty(i_tmp)
+	disp(var_list(i,:));
+	error (['One of the variable specified does not exist']) ;
+      else
+	ivar(i) = i_tmp;
+      end
+    end
+  end
 
-
+  for i=1:nvar
+    my_subplot(i,nvar,2,3,'Forecasts');
+    
+    plot([-ykmin_+1:0],y0(ivar(i),1:ykmin_),'b-',...
+	 [1:k],yf(ivar(i),ykmin_+1:end),'g-',...
+	 [1:k],yf(ivar(i),ykmin_+1:end)'+2*sqrt(var_yf(:,ivar(i))),'g:',...
+	 [1:k],yf(ivar(i),ykmin_+1:end)'-2*sqrt(var_yf(:,ivar(i))),'g:',...
+	 [1 k],repmat(ys_(ivar(i),1,2)'r-');
+    title(lgy_(ivar(i),:));
+  end
 
 
 
