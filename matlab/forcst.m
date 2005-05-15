90788ccd173132c92879ced8e88a086f3f2c7d7c
michel <michel@ac1d8469-bf42-47a9-8791-bf33cf982152>
2005-05-15 14:37:25 +0000

    use kalman_transition_matrix() to buid transition matrix. Maybe less efficient but clearer and it fixess a bug
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v3@206 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/forcst.m
+++ matlab/forcst.m
@@ -5,60 +5,20 @@ function [yf,var_yf]=forcst(dr,y0,k,var_list)
   make_ex_;
   yf = simult_(y0,dr,ex_(1:k,:),1);
 
-  nstatic = dr.nstatic;
-  npred = dr.npred;
-  j = find(dr.kstate(dr.kae,2) <= ykmin_+1);
-  kae = dr.kae(j);
-  nh = size(dr.ghx,2);
-  hx = dr.ghx(nstatic+1:nstatic+npred,:);
-  hu = dr.ghu(nstatic+1:nstatic+npred,:);
-  if ~isempty(kae)
-    n = length(kae);
-    tmp = sparse([1:n]',kae-sum(dr.kstate(:,2)>ykmin_+1),ones(n,1),n,nh);
-    hx = [hx; tmp];
-    hu = [hu; zeros(n,exo_nbr)];
-  end
-  gx  = [];
-  k2  = [];
-  if nstatic > 0
-    gx = dr.ghx(1:nstatic,:);
-    k2 = [1:nstatic]';
-  end
-  if size(dr.ghx,1) > nstatic+npred
-    gx = [gx; dr.ghx(nstatic+npred+1:end,:)];
-    k2 = [k2; [nstatic+npred+1:size(dr.ghx,1)]'];
-  end
+  [A,B] = kalman_transition_matrix(dr);
   
-  k1 = dr.order_var([nstatic+1:nstatic+npred]);
-  k2 = dr.order_var(k2);
+  sigma_u = B*Sigma_e_*B';
+  sigma_y = 0;
   
-  sigma_u = hu*Sigma_e_*hu';
-  sigma_y1 = 0;
-  var_yf = zeros(k,endo_nbr);
-  
+  for i=1:k
+    sigma_y = sigma_y+sigma_u;
+    var_yf(i,dr.order_var) = diag(sigma_y(1:endo_nbr,1:endo_nbr))';
+    if i == k
+      break
+    end
+    sigma_u = A*sigma_u*A';
+  end
 
-  if isempty(k2)
-    for i=1:k
-      sigma_y1 = sigma_y1+sigma_u;
-      var_yf(i,k1) = diag(sigma_y1(1:npred,1:npred))';
-      if i == k
-	break
-      end
-      sigma_u = hx*sigma_u*hx';
-    end
-  else
-    for i=1:k
-      sigma_y1 = sigma_y1+sigma_u;
-      var_yf(i,k1) = diag(sigma_y1(1:npred,1:npred))';
-      sigma_y2 = gx*sigma_y1*gx';
-      var_yf(i,k2) = diag(sigma_y2)';
-      if i == k
-	  break
-      end
-      sigma_u = hx*sigma_u*hx';
-    end
-  end
-  
   nvar = size(var_list,1);
   if nvar == 0
     nvar = endo_nbr;
@@ -83,7 +43,7 @@ function [yf,var_yf]=forcst(dr,y0,k,var_list)
 	 [1:k],yf(ivar(i),ykmin_+1:end),'g-',...
 	 [1:k],yf(ivar(i),ykmin_+1:end)'+2*sqrt(var_yf(:,ivar(i))),'g:',...
 	 [1:k],yf(ivar(i),ykmin_+1:end)'-2*sqrt(var_yf(:,ivar(i))),'g:',...
-	 [1 k],repmat(ys_(ivar(i),1,2)'r-');
+	 [1 k],repmat(dr.ys(ivar(i)),1,2),'r-');
     title(lgy_(ivar(i),:));
   end
 
