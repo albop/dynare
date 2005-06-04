216bd8ae995daab1b1d208473993bb4647a1b78a
michel <michel@ac1d8469-bf42-47a9-8791-bf33cf982152>
2005-06-04 14:59:09 +0000

    removed c and ncc as global; ncc is replaced by jcf
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v3@266 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/bksupk.m
+++ matlab/bksupk.m
@@ -1,16 +1,15 @@
 function d1 = bksupk(ny,fid,jcf,icc1)
 
-global ykmax_ iter_ c ncc
+global ykmax_ iter_ jcf
 
 icf = [1:jcf-1] ;
 ir = [(iter_-1)*ny+1:ny*iter_] ;
 irf = icc1+(iter_-1)*ny ;
 d1 = zeros(iter_*ny,1) ;
 
-ofs = (((iter_-1)*ny+1)-1)*ncc*8 ;
+ofs = (((iter_-1)*ny+1)-1)*jcf*8 ;
 junk = fseek(fid,ofs,-1) ;
-c = fread(fid,[ncc,ny],'float64') ;
-c = c' ;
+c = fread(fid,[jcf,ny],'float64')' ;
 
 d1(ir) = c(:,jcf) ;
 ir = ir-ny ;
@@ -20,10 +19,9 @@ i = 2 ;
 while i <= ykmax_ | i <= iter_
 	irf1 = selif(irf,irf<=iter_*ny) ;
 
-	ofs = (((iter_-i)*ny+1)-1)*ncc*8 ;
+	ofs = (((iter_-i)*ny+1)-1)*jcf*8 ;
 	junk = fseek(fid,ofs,-1) ;
-	c = fread(fid,[ncc,ny],'float64') ;
-	c = c' ;
+	c = fread(fid,[jcf,ny],'float64')' ;
 
 	d1(ir) = c(:,jcf) - c(:,1:size(irf1,1))*d1(irf1) ;
 	ir = ir - ny ;
@@ -33,10 +31,9 @@ end
 
 while i <= iter_
 
-	ofs = (((iter_-i)*ny+1)-1)*ncc*8 ;
+	ofs = (((iter_-i)*ny+1)-1)*jcf*8 ;
 	junk = fseek(fid,ofs,-1) ;
-	c = fread(fid,[ncc,ny],'float64') ;
-	c = c' ;
+	c = fread(fid,[jcf,ny],'float64')' ;
 
 	d1(ir) = c(:,jcf)-c(:,icf)*d1(irf) ;
 	ir = ir-ny ;			
@@ -44,4 +41,3 @@ while i <= iter_
 	i = i+1;
 end
 
-return ;
