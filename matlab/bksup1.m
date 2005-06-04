bdcb91c2a8fd4c8da2f7e936e31704ba90263a5d
michel <michel@ac1d8469-bf42-47a9-8791-bf33cf982152>
2005-06-04 14:23:56 +0000

    make that c isn't modified in the function
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v3@265 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/bksup1.m
+++ matlab/bksup1.m
@@ -7,13 +7,11 @@ global iter_
 ir = [(iter_-2)*ny+1:ny+(iter_-2)*ny] ;
 irf = iyf+(iter_-1)*ny ;
 icf = [1:size(iyf,2)] ;
+d = c(:,jcf) ;
 
 for i = 2:iter_
-	c(ir,jcf) = c(ir,jcf)-c(ir,icf)*c(irf,jcf) ;
+	d(ir) = c(ir,jcf)-c(ir,icf)*d(irf) ;
 	ir = ir-ny ;
 	irf = irf-ny ;
 end
 
-d = c(:,jcf) ;
-
-return ;
