746e59239223c4da7dc108d73d10848ab482bb79
michel <michel@ac1d8469-bf42-47a9-8791-bf33cf982152>
2005-06-04 15:47:02 +0000

    changed algorithm to compute variance and corrected bug for models with lags on more than 1 period
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v3@269 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/osr_obj.m
+++ matlab/osr_obj.m
@@ -1,6 +1,6 @@
 % the beginning and the end of this function may be adapted by the userx
 function [loss,vx]=osr_obj(x,params,weights);
-  global ys_ Sigma_e_ endo_nbr exo_nbr optimal_Q_ it_ ykmin_
+  global ys_ Sigma_e_ endo_nbr exo_nbr optimal_Q_ it_ ykmin_ options_
   
   vx = [];
   % set parameters of the policiy rule
@@ -13,7 +13,7 @@ function [loss,vx]=osr_obj(x,params,weights);
   it_ = ykmin_+1;
   [dr_,info] = resol(ys_,0);
   
-  switch info
+  switch info(1)
    case 1
     loss = 1e8;
     return
@@ -35,36 +35,10 @@ function [loss,vx]=osr_obj(x,params,weights);
    otherwise
   end
   
-  nstatic = dr_.nstatic;
-  npred = dr_.npred;
-  ghx = dr_.ghx;
-  ghu = dr_.ghu;
-  order = dr_.order_var;
-  k=[nstatic+1:nstatic+npred]';
-  vx1 = ghu(k,:)*Sigma_e_*ghu(k,:)';
-  
-  % compute variance of predetermined variables
-  vx = (eye(npred*npred)-kron(ghx(k,:),dr_.ghx(k,:)))\vx1(:);
-  vx=reshape(vx,npred,npred);
-
-  % compute variance of all variables
-  if endo_nbr > npred
-    qx = eye(npred);
-    qu = zeros(npred,exo_nbr);
-    if nstatic > 0
-      qx = [ghx(1:nstatic,:);qx];
-      qu = [ghu(1:nstatic,:);qu];
-    end
-    if endo_nbr > nstatic+npred
-      qx = [qx;ghx(nstatic+npred+1:end,:)];
-      qu = [qu;ghu(nstatic+npred+1:end,:)];
-    end
-    vx = qx*vx*qx'+qu*Sigma_e_*qu';
-  end
-  % end of the non touch region of the program
-  
-  % computes the loss function
-  weights = weights(order,order);
+  [A,B] = kalman_transition_matrix(dr_);
+  vx = lyapunov_symm(A,B*Sigma_e_*B');
+  weights = weights(dr_.order_var,dr_.order_var);
+  vx = vx(1:endo_nbr,1:endo_nbr);
   loss = weights(:)'*vx(:);
   
 
