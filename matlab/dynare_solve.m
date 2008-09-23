2d83b2216d7b9518718efd83808a1d7c47c488dd
sebastien <sebastien@ac1d8469-bf42-47a9-8791-bf33cf982152>
2008-09-23 09:06:03 +0000

    4.0: merged changesets 2078, 2089 and 2090 from trunk * new debug messages in dynare_solve.m * solve_algo=2 exits immediately after failure to solve one block * new solve_algo=4 with different way of dealing with badly scaled or nearly singular Jacobian * solve_algo=0 fails if Matlab's Optimization Toolbox not present * dynare_solve fails if solve_algo not in [0,1,2,3,4]
    git-svn-id: https://www.dynare.org/svn/dynare/branches/4.0@2095 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/dynare_solve.m
+++ matlab/dynare_solve.m
@@ -38,33 +38,31 @@ function [x,info] = dynare_solve(func,x,jacobian_flag,varargin)
   options_ = set_default_option(options_,'solve_algo',2);
   info = 0;
   if options_.solve_algo == 0
-    if ~isempty(which('fsolve'))
-      options=optimset('fsolve');
-      options.MaxFunEvals = 50000;
-      options.MaxIter = 2000;
-      options.TolFun=1e-8;
-      options.Display = 'iter';
-      if jacobian_flag
-	options.Jacobian = 'on';
-      else
-	options.Jacobian = 'off';
-      end
-      [x,fval,exitval,output] = fsolve(func,x,options,varargin{:});
-      if exitval > 0
-	info = 0;
-      else
-	info = 1;
-      end
-      return
-    else 
-      options_.solve_algo = 1;
+    if exist('OCTAVE_VERSION') || isempty(ver('optim'))
+      % Note that fsolve() exists under Octave, but has a different syntax
+      % So we fail for the moment under Octave, until we add the corresponding code
+      error('DYNARE_SOLVE: you can''t use solve_algo=0 since you don''t have Matlab''s Optimization Toolbox')
     end
-  end
-
-  if options_.solve_algo == 1
+    options=optimset('fsolve');
+    options.MaxFunEvals = 50000;
+    options.MaxIter = 2000;
+    options.TolFun=1e-8;
+    options.Display = 'iter';
+    if jacobian_flag
+      options.Jacobian = 'on';
+    else
+      options.Jacobian = 'off';
+    end
+    [x,fval,exitval,output] = fsolve(func,x,options,varargin{:});
+    if exitval > 0
+      info = 0;
+    else
+      info = 1;
+    end
+  elseif options_.solve_algo == 1
     nn = size(x,1);
     [x,info]=solve1(func,x,1:nn,1:nn,jacobian_flag,varargin{:});
-  elseif options_.solve_algo == 2
+  elseif options_.solve_algo == 2 || options_.solve_algo == 4
     nn = size(x,1) ;
     tolf = options_.solve_tolf ;
 
@@ -84,8 +82,6 @@ function [x,info] = dynare_solve(func,x,jacobian_flag,varargin)
       error('exiting ...')
     end
     
-%    f = 0.5*fvec'*fvec ;
-
     if max(abs(fvec)) < tolf
       return ;
     end
@@ -102,25 +98,37 @@ function [x,info] = dynare_solve(func,x,jacobian_flag,varargin)
 
     [j1,j2,r,s] = dmperm(fjac);
     
+    if options_.debug
+      disp(['DYNARE_SOLVE (solve_algo=2|4): number of blocks = ' num2str(length(r))]);
+    end
+    
     for i=length(r)-1:-1:1
-      [x,info]=solve1(func,x,j1(r(i):r(i+1)-1),j2(r(i):r(i+1)-1),jacobian_flag,varargin{:});
-      if info & options_.debug
-	error(sprintf('Solve block = %d check = %d\n',i,info));
+      if options_.debug
+        disp(['DYNARE_SOLVE (solve_algo=2|4): solving block ' num2str(i) ', of size ' num2str(r(i+1)-r(i)) ]);
+      end
+      if options_.solve_algo == 2
+        [x,info]=solve1(func,x,j1(r(i):r(i+1)-1),j2(r(i):r(i+1)-1),jacobian_flag,varargin{:});
+      else % solve_algo=4
+        [x,info]=solve2(func,x,j1(r(i):r(i+1)-1),j2(r(i):r(i+1)-1),jacobian_flag,varargin{:});
+      end
+      if info
+        return
       end
     end
     fvec = feval(func,x,varargin{:});
     if max(abs(fvec)) > tolf
-      [x,info]=solve1(func,x,1:nn,1:nn,jacobian_flag,varargin{:});
+      if options_.solve_algo == 2
+        [x,info]=solve1(func,x,1:nn,1:nn,jacobian_flag,varargin{:});
+      else % solve_algo=4
+        [x,info]=solve2(func,x,1:nn,1:nn,jacobian_flag,varargin{:});
+      end
     end
   elseif options_.solve_algo == 3
     if jacobian_flag
       [x,info] = csolve(func,x,func,1e-6,500,varargin{:});
     else
       [x,info] = csolve(func,x,[],1e-6,500,varargin{:});
-    end 
+    end
+  else
+    error('DYNARE_SOLVE: option solve_algo must be one of [0,1,2,3,4]')
   end
-%    fvec1 = feval(func,x,varargin{:})
-
-  % 08/28/03 MJ add a final call to solve1 for solve_algo == 1 in case
-  %             initvals generates 'false' zeros in the Jacobian
-  
