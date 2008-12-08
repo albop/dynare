9ed5c34d097a59d731ff13914c4d5575ae4eb9f0
sebastien <sebastien@ac1d8469-bf42-47a9-8791-bf33cf982152>
2008-12-08 16:20:55 +0000

    4.0: merged r2301 changeset (fixed dsample command)
    git-svn-id: https://www.dynare.org/svn/dynare/branches/4.0@2302 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/global_initialization.m
+++ matlab/global_initialization.m
@@ -198,10 +198,6 @@ function global_initialization()
   % BVAR
   M_.bvar = [];
 
-  % rplot
-
-  options_.dsample = 0;
-  
   % homotopy
   options_.homotopy_mode = 0;
   options_.homotopy_steps = 1;
