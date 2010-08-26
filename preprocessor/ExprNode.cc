albop/dynare

commit db3702f559448157287343fdf1cc95aa8896012c

Preprocessor: when removing lags greater than 2 on endogenous or lags on exogenous, don't substitute model local variables that do not need to (cherry picked from commit e5ce0264bff270a2f913087f6e4b7ba69264f24e)

preprocessor/ExprNode.cc
@@ -319,6 +319,18 @@ NumConstNode::maxExoLead() const
   return 0;
 }
 
+int
+NumConstNode::maxEndoLag() const
+{
+  return 0;
+}
+
+int
+NumConstNode::maxExoLag() const
+{
+  return 0;
+}
+
 NodeID
 NumConstNode::decreaseLeadsLags(int n) const
 {
@@ -836,6 +848,34 @@ VariableNode::maxExoLead() const
     }
 }
 
+int
+VariableNode::maxEndoLag() const
+{
+  switch (type)
+    {
+    case eEndogenous:
+      return max(-lag, 0);
+    case eModelLocalVariable:
+      return datatree.local_variables_table[symb_id]->maxEndoLag();
+    default:
+      return 0;
+    }
+}
+
+int
+VariableNode::maxExoLag() const
+{
+  switch (type)
+    {
+    case eExogenous:
+      return max(-lag, 0);
+    case eModelLocalVariable:
+      return datatree.local_variables_table[symb_id]->maxExoLag();
+    default:
+      return 0;
+    }
+}
+
 NodeID
 VariableNode::decreaseLeadsLags(int n) const
 {
@@ -887,6 +927,7 @@ NodeID
 VariableNode::substituteEndoLagGreaterThanTwo(subst_table_t &subst_table, vector<BinaryOpNode *> &neweqs) const
 {
   VariableNode *substexpr;
+  NodeID value;
   subst_table_t::const_iterator it;
   int cur_lag;
   switch (type)
@@ -923,7 +964,11 @@ VariableNode::substituteEndoLagGreaterThanTwo(subst_table_t &subst_table, vector
       return substexpr;
 
     case eModelLocalVariable:
-      return datatree.local_variables_table[symb_id]->substituteEndoLagGreaterThanTwo(subst_table, neweqs);
+      value = datatree.local_variables_table[symb_id];
+      if (value->maxEndoLag() <= 1)
+        return const_cast<VariableNode *>(this);
+      else
+        return value->substituteEndoLagGreaterThanTwo(subst_table, neweqs);
     default:
       return const_cast<VariableNode *>(this);
     }
@@ -955,6 +1000,7 @@ NodeID
 VariableNode::substituteExoLag(subst_table_t &subst_table, vector<BinaryOpNode *> &neweqs) const
 {
   VariableNode *substexpr;
+  NodeID value;
   subst_table_t::const_iterator it;
   int cur_lag;
   switch (type)
@@ -991,7 +1037,11 @@ VariableNode::substituteExoLag(subst_table_t &subst_table, vector<BinaryOpNode *
       return substexpr;
 
     case eModelLocalVariable:
-      return datatree.local_variables_table[symb_id]->substituteExoLag(subst_table, neweqs);
+      value = datatree.local_variables_table[symb_id];
+      if (value->maxExoLag() == 0)
+        return const_cast<VariableNode *>(this);
+      else
+        return value->substituteExoLag(subst_table, neweqs);
     default:
       return const_cast<VariableNode *>(this);
     }
@@ -1667,6 +1717,18 @@ int
 UnaryOpNode::maxExoLead() const
 {
   return arg->maxExoLead();
+}
+
+int
+UnaryOpNode::maxEndoLag() const
+{
+  return arg->maxEndoLag();
+}
+
+int
+UnaryOpNode::maxExoLag() const
+{
+  return arg->maxExoLag();
 }
 
 NodeID
@@ -2659,6 +2721,18 @@ BinaryOpNode::maxExoLead() const
   return max(arg1->maxExoLead(), arg2->maxExoLead());
 }
 
+int
+BinaryOpNode::maxEndoLag() const
+{
+  return max(arg1->maxEndoLag(), arg2->maxEndoLag());
+}
+
+int
+BinaryOpNode::maxExoLag() const
+{
+  return max(arg1->maxExoLag(), arg2->maxExoLag());
+}
+
 NodeID
 BinaryOpNode::decreaseLeadsLags(int n) const
 {
@@ -3149,6 +3223,18 @@ TrinaryOpNode::maxExoLead() const
   return max(arg1->maxExoLead(), max(arg2->maxExoLead(), arg3->maxExoLead()));
 }
 
+int
+TrinaryOpNode::maxEndoLag() const
+{
+  return max(arg1->maxEndoLag(), max(arg2->maxEndoLag(), arg3->maxEndoLag()));
+}
+
+int
+TrinaryOpNode::maxExoLag() const
+{
+  return max(arg1->maxExoLag(), max(arg2->maxExoLag(), arg3->maxExoLag()));
+}
+
 NodeID
 TrinaryOpNode::decreaseLeadsLags(int n) const
 {
@@ -3370,6 +3456,26 @@ UnknownFunctionNode::maxExoLead() const
   for (vector<NodeID>::const_iterator it = arguments.begin();
        it != arguments.end(); it++)
     val = max(val, (*it)->maxExoLead());
+  return val;
+}
+
+int
+ExternalFunctionNode::maxEndoLag() const
+{
+  int val = 0;
+  for (vector<NodeID>::const_iterator it = arguments.begin();
+       it != arguments.end(); it++)
+    val = max(val, (*it)->maxEndoLag());
+  return val;
+}
+
+int
+ExternalFunctionNode::maxExoLag() const
+{
+  int val = 0;
+  for (vector<NodeID>::const_iterator it = arguments.begin();
+       it != arguments.end(); it++)
+    val = max(val, (*it)->maxExoLag());
   return val;
 }
 

preprocessor/ExprNode.hh
@@ -237,6 +237,14 @@ public:
   /*! Always returns a non-negative value */
   virtual int maxExoLead() const = 0;
 
+  //! Returns the maximum lag of endogenous in this expression
+  /*! Always returns a non-negative value */
+  virtual int maxEndoLag() const = 0;
+
+  //! Returns the maximum lag of exogenous in this expression
+  /*! Always returns a non-negative value */
+  virtual int maxExoLag() const = 0;
+
   //! Returns a new expression where all the leads/lags have been shifted backwards by the same amount
   /*!
     Only acts on endogenous, exogenous, exogenous det
@@ -363,6 +371,8 @@ public:
   virtual NodeID getChainRuleDerivative(int deriv_id, const map<int, NodeID> &recursive_variables);
   virtual int maxEndoLead() const;
   virtual int maxExoLead() const;
+  virtual int maxEndoLag() const;
+  virtual int maxExoLag() const;
   virtual NodeID decreaseLeadsLags(int n) const;
   virtual NodeID substituteEndoLeadGreaterThanTwo(subst_table_t &subst_table, vector<BinaryOpNode *> &neweqs) const;
   virtual NodeID substituteEndoLagGreaterThanTwo(subst_table_t &subst_table, vector<BinaryOpNode *> &neweqs) const;
@@ -381,6 +391,7 @@ private:
   //! Id from the symbol table
   const int symb_id;
   const SymbolType type;
+  //! A positive value is a lead, a negative is a lag
   const int lag;
   virtual NodeID computeDerivative(int deriv_id);
 public:
@@ -407,6 +418,8 @@ public:
   virtual NodeID getChainRuleDerivative(int deriv_id, const map<int, NodeID> &recursive_variables);
   virtual int maxEndoLead() const;
   virtual int maxExoLead() const;
+  virtual int maxEndoLag() const;
+  virtual int maxExoLag() const;
   virtual NodeID decreaseLeadsLags(int n) const;
   virtual NodeID substituteEndoLeadGreaterThanTwo(subst_table_t &subst_table, vector<BinaryOpNode *> &neweqs) const;
   virtual NodeID substituteEndoLagGreaterThanTwo(subst_table_t &subst_table, vector<BinaryOpNode *> &neweqs) const;
@@ -463,6 +476,8 @@ public:
   virtual NodeID getChainRuleDerivative(int deriv_id, const map<int, NodeID> &recursive_variables);
   virtual int maxEndoLead() const;
   virtual int maxExoLead() const;
+  virtual int maxEndoLag() const;
+  virtual int maxExoLag() const;
   virtual NodeID decreaseLeadsLags(int n) const;
   virtual NodeID substituteEndoLeadGreaterThanTwo(subst_table_t &subst_table, vector<BinaryOpNode *> &neweqs) const;
   //! Creates another UnaryOpNode with the same opcode, but with a possibly different datatree and argument
@@ -528,6 +543,8 @@ public:
   virtual NodeID getChainRuleDerivative(int deriv_id, const map<int, NodeID> &recursive_variables);
   virtual int maxEndoLead() const;
   virtual int maxExoLead() const;
+  virtual int maxEndoLag() const;
+  virtual int maxExoLag() const;
   virtual NodeID decreaseLeadsLags(int n) const;
   virtual NodeID substituteEndoLeadGreaterThanTwo(subst_table_t &subst_table, vector<BinaryOpNode *> &neweqs) const;
   //! Creates another BinaryOpNode with the same opcode, but with a possibly different datatree and arguments
@@ -575,6 +592,8 @@ public:
   virtual NodeID getChainRuleDerivative(int deriv_id, const map<int, NodeID> &recursive_variables);
   virtual int maxEndoLead() const;
   virtual int maxExoLead() const;
+  virtual int maxEndoLag() const;
+  virtual int maxExoLag() const;
   virtual NodeID decreaseLeadsLags(int n) const;
   virtual NodeID substituteEndoLeadGreaterThanTwo(subst_table_t &subst_table, vector<BinaryOpNode *> &neweqs) const;
   //! Creates another TrinaryOpNode with the same opcode, but with a possibly different datatree and arguments
@@ -616,6 +635,8 @@ public:
   virtual NodeID getChainRuleDerivative(int deriv_id, const map<int, NodeID> &recursive_variables);
   virtual int maxEndoLead() const;
   virtual int maxExoLead() const;
+  virtual int maxEndoLag() const;
+  virtual int maxExoLag() const;
   virtual NodeID decreaseLeadsLags(int n) const;
   virtual NodeID substituteEndoLeadGreaterThanTwo(subst_table_t &subst_table, vector<BinaryOpNode *> &neweqs) const;
   virtual NodeID substituteEndoLagGreaterThanTwo(subst_table_t &subst_table, vector<BinaryOpNode *> &neweqs) const;

commit a906a435ed2a7958c80dfb5c0de3268801ea6b12

Preprocessor: fix error introduced in db3702f559448157287343fdf1cc95aa8896012c

preprocessor/ExprNode.cc
@@ -3460,7 +3460,7 @@ UnknownFunctionNode::maxExoLead() const
 }
 
 int
-ExternalFunctionNode::maxEndoLag() const
+UnknownFunctionNode::maxEndoLag() const
 {
   int val = 0;
   for (vector<NodeID>::const_iterator it = arguments.begin();
@@ -3470,7 +3470,7 @@ ExternalFunctionNode::maxEndoLag() const
 }
 
 int
-ExternalFunctionNode::maxExoLag() const
+UnknownFunctionNode::maxExoLag() const
 {
   int val = 0;
   for (vector<NodeID>::const_iterator it = arguments.begin();