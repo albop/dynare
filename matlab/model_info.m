albop/dynare

commit 7350d0cfe16c0b8804754eda76461361b57f5bfb

correction of model_info's printouts (cherry picked from commit f268513ffba2c9911e183e5364a6d0f378574f33)

matlab/model_info.m
@@ -44,7 +44,7 @@ if(isfield(M_,'block_structure'))
     fprintf('\n');
     for k=1:M_.maximum_endo_lag+M_.maximum_endo_lead+1
         if(k==M_.maximum_endo_lag+1)
-            fprintf('%-30s %s','the variable','is used in equations ontemporaneously');
+            fprintf('%-30s %s','the variable','is used in equations Contemporaneously');
         elseif(k<M_.maximum_endo_lag+1)
             fprintf('%-30s %s %d','the variable','is used in equations with lag ',M_.maximum_endo_lag+1-k);
         else
@@ -56,7 +56,7 @@ if(isfield(M_,'block_structure'))
             IM=[];
         end;
         size_IM=size(IM,1);
-        last=0;
+        last=99999999;
         for i=1:size_IM
             if(last~=IM(i,2))
                 fprintf('\n%-30s',M_.endo_names(IM(i,2),:));

commit c6952a4e508812e802acb9d3c4c1cb1d282d5507

Block decomposition: revert bug introduced in 7350d0cfe16c0b8804754eda76461361b57f5bfb

matlab/model_info.m
@@ -56,7 +56,7 @@ if(isfield(M_,'block_structure'))
             IM=[];
         end;
         size_IM=size(IM,1);
-        last=99999999;
+        last=0;
         for i=1:size_IM
             if(last~=IM(i,2))
                 fprintf('\n%-30s',M_.endo_names(IM(i,2),:));