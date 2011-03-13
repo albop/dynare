MichelJuillard/dynare-michel

commit 32c997f1ef7cc6b8f8a156f012f78c64b4f25234

correcting bug when some variables don't appear at the current period cleaning set_state_space.m, taking into account that all modesl have leads and lags on only one period.

matlab/set_state_space.m
@@ -32,41 +32,41 @@ function dr=set_state_space(dr,M_)
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
 
-xlen = M_.maximum_endo_lead + M_.maximum_endo_lag + 1;
-klen = M_.maximum_endo_lag + M_.maximum_endo_lead + 1;
+max_lead = M_.maximum_endo_lead;
+max_lag = M_.maximum_endo_lag;
+endo_nbr = M_.endo_nbr;
+lead_lag_incidence = M_.lead_lag_incidence;
+xlen = max_lead + max_lag + 1;
+klen = max_lag + max_lead + 1;
 
-if ~ M_.lead_lag_incidence(M_.maximum_endo_lag+1,:) > 0
-    error ('Error in model specification: some variables don"t appear as current') ;
-end
-
-fwrd_var = find(any(M_.lead_lag_incidence(M_.maximum_endo_lag+2:end,:),1))';
-if M_.maximum_endo_lag > 0
-    pred_var = find(any(M_.lead_lag_incidence(1:M_.maximum_endo_lag,:),1))';
+fwrd_var = find(any(lead_lag_incidence(max_lag+2:end,:),1))';
+if max_lag > 0
+    pred_var = find(any(lead_lag_incidence(1,:),1))';
     both_var = intersect(pred_var,fwrd_var);
     pred_var = setdiff(pred_var,both_var);
     fwrd_var = setdiff(fwrd_var,both_var);
-    stat_var = setdiff([1:M_.endo_nbr]',union(union(pred_var,both_var),fwrd_var));  % static variables
+    stat_var = setdiff([1:endo_nbr]',union(union(pred_var,both_var),fwrd_var));  % static variables
 else
     pred_var = [];
     both_var = [];
-    stat_var = setdiff([1:M_.endo_nbr]',fwrd_var);
+    stat_var = setdiff([1:endo_nbr]',fwrd_var);
 end
 nboth = length(both_var);
 npred = length(pred_var);
 nfwrd = length(fwrd_var);
 nstatic = length(stat_var);
 order_var = [ stat_var(:); pred_var(:); both_var(:); fwrd_var(:)];
-inv_order_var(order_var) = (1:M_.endo_nbr);
+inv_order_var(order_var) = (1:endo_nbr);
 
 % building kmask for z state vector in t+1
-if M_.maximum_endo_lag > 0
+if max_lag > 0
     kmask = [];
-    if M_.maximum_endo_lead > 0 
-        kmask = [cumsum(flipud(M_.lead_lag_incidence(M_.maximum_endo_lag+2:end,order_var)),1)] ;
+    if max_lead > 0 
+        kmask = [cumsum(flipud(lead_lag_incidence(max_lag+2:end,order_var)),1)] ;
     end
-    kmask = [kmask; flipud(cumsum(M_.lead_lag_incidence(1:M_.maximum_endo_lag,order_var),1))] ;
+    kmask = [kmask; flipud(cumsum(lead_lag_incidence(1,order_var),1))] ;
 else
-    kmask = cumsum(flipud(M_.lead_lag_incidence(M_.maximum_endo_lag+2:klen,order_var)),1) ;
+    kmask = cumsum(flipud(lead_lag_incidence(max_lag+2:klen,order_var)),1) ;
 end
 
 kmask = kmask';
@@ -75,27 +75,16 @@ i_kmask = find(kmask);          % index of nonzero entries in kmask
 nd = size(i_kmask,1);           % size of the state vector
 kmask(i_kmask) = [1:nd];
 
-% auxiliary equations
-
-% elements that are both in z(t+1) and z(t)
-k1 = find([kmask(1:end-M_.endo_nbr) & kmask(M_.endo_nbr+1:end)] );
-kad = [];
-kae = [];
-if ~isempty(k1)
-    kad = kmask(k1+M_.endo_nbr);
-    kae = kmask(k1);
-end
-
 % composition of state vector
 % col 1: variable;           col 2: lead/lag in z(t+1); 
 % col 3: A cols for t+1 (D); col 4: A cols for t (E)
-kstate = [ repmat([1:M_.endo_nbr]',klen-1,1) kron([klen:-1:2]',ones(M_.endo_nbr,1)) ...
-           zeros((klen-1)*M_.endo_nbr,2)];
-kiy = flipud(M_.lead_lag_incidence(:,order_var))';
+kstate = [ repmat([1:endo_nbr]',klen-1,1) kron([klen:-1:2]',ones(endo_nbr,1)) ...
+           zeros((klen-1)*endo_nbr,2)];
+kiy = flipud(lead_lag_incidence(:,order_var))';
 kiy = kiy(:);
-kstate(1:M_.maximum_endo_lead*M_.endo_nbr,3) = kiy(1:M_.maximum_endo_lead*M_.endo_nbr)-M_.endo_nbr;  
+kstate(1:endo_nbr,3) = kiy(1:endo_nbr)-nnz(lead_lag_incidence(2,:));  
 kstate(find(kstate(:,3) < 0),3) = 0;
-kstate(M_.maximum_endo_lead*M_.endo_nbr+1:end,4) = kiy((M_.maximum_endo_lead+1)*M_.endo_nbr+1:end);  
+kstate(endo_nbr+1:end,4) = kiy(2*endo_nbr+1:end);  
 % put in E only the current variables that are not already in D
 kstate = kstate(i_kmask,:);
 
@@ -104,28 +93,13 @@ dr.inv_order_var = inv_order_var';
 dr.nstatic = nstatic;
 dr.npred = npred+nboth;
 dr.kstate = kstate;
-dr.kad = kad;
-dr.kae = kae;
+dr.kad = [];
+dr.kae = [];
 dr.nboth = nboth;
 dr.nfwrd = nfwrd;
 % number of forward variables in the state vector
-dr.nsfwrd = sum(kstate(:,2) > M_.maximum_endo_lag+1);
+dr.nsfwrd = nfwrd;
 % number of predetermined variables in the state vector
-dr.nspred = sum(kstate(:,2) <= M_.maximum_endo_lag+1);
+dr.nspred = npred;
 
-% computes column position of auxiliary variables for 
-% compact transition matrix (only state variables)
-aux = zeros(0,1);
-k0 = kstate(find(kstate(:,2) <= M_.maximum_endo_lag+1),:);;
-i0 = find(k0(:,2) == M_.maximum_endo_lag+1);
-for i=M_.maximum_endo_lag:-1:2
-    i1 = find(k0(:,2) == i);
-    n1 = size(i1,1);
-    j = zeros(n1,1);
-    for j1 = 1:n1
-        j(j1) = find(k0(i0,1)==k0(i1(j1),1));
-    end
-    aux = [aux; i0(j)];
-    i0 = i1;
-end
-dr.transition_auxiliary_variables = [(1:size(aux,1))' aux];
+dr.transition_auxiliary_variables = [];

albop/dynare

commit 5592dc0096dfbe3c5e3f6f503519f3868097129e

corrected bug introduced in changeset 32c997f1ef7cc6b8f8a156f012f78c64b4f25234 of 02/25/11 and affecting k_order_perturbation (cherry picked from commit 96b7e1f599b7c69804b6838a32ed3b2208b9aa86)

matlab/set_state_space.m
@@ -110,7 +110,7 @@ dr.kae = kae;
 dr.nboth = nboth;
 dr.nfwrd = nfwrd;
 % number of forward variables in the state vector
-dr.nsfwrd = nfwrd;
+dr.nsfwrd = nfwrd+nboth;
 % number of predetermined variables in the state vector
 dr.nspred = npred+nboth;
 