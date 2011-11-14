c1e9b3f822789944980009c57babd4dbeb07ca6c
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2011-11-14 17:56:27 +0100

    Do not plot the priors if option nograph is used. (cherry picked from commit bcf7e8821730b2e0916e71b3198f3eb734e73b8e)
    Conflicts:
    
    	matlab/dynare_estimation_init.m

--- matlab/dynare_estimation_init.m
+++ matlab/dynare_estimation_init.m
@@ -124,7 +124,7 @@ if ~isempty(estim_params_)
     [xparam1,estim_params_,bayestopt_,lb,ub,M_] = set_prior(estim_params_,M_,options_);
     if any(bayestopt_.pshape > 0)
         % Plot prior densities.
-        if options_.plot_priors
+        if ~options_.nograph && options_.plot_priors
             plot_priors(bayestopt_,M_,options_)
         end
         % Set prior bounds
