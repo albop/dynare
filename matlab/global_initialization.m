b03697b3429f8570b5f855538e404aa17baae1c0
Johannes Pfeifer <jpfeifer@gmx,de>
2013-10-30 21:43:33 +0100

    Add possibility to initialize parameters from calibrated model

--- matlab/global_initialization.m
+++ matlab/global_initialization.m
@@ -381,6 +381,7 @@ options_.mh_nblck = 2;
 options_.mh_recover = 0;
 options_.mh_replic = 20000;
 options_.recursive_estimation_restart = 0;
+options_.use_calibration_initialization = 0;
 
 options_.mode_compute = 4;
 options_.mode_file = '';
