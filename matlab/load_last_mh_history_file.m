13935647011eb522f3ede9f4fa7390eb600f0880
Stéphane Adjemian (Scylla) <stephane.adjemian@univ-lemans.fr>
2013-11-21 15:24:35 +0100

    Fixed bug of unknown origin...

--- matlab/load_last_mh_history_file.m
+++ matlab/load_last_mh_history_file.m
@@ -16,45 +16,39 @@ function info = load_last_mh_history_file(MetropolisFolder, ModelName)
 %
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
-    
-BaseName = [MetropolisFolder filesep ModelName];
-    
+
+% record is also a Matlab function.
+record = 0;
+
 % Get the list of all the mh_history files.
+BaseName = [MetropolisFolder filesep ModelName];
 mh_history_files = dir([BaseName '_mh_history_*.mat']);
 
 % Consistency with older versions of Dynare.
 if isequal(length(mh_history_files),0)
     if exist([BaseName '_mh_history.mat'])
-        copyfile([BaseName '_mh_history.mat'],[BaseName '_mh_history_0.mat'])
-        version = 1;
-        save([BaseName '_mh_history_0.mat'],'version','-append');
-        mh_history_files = dir([BaseName '_mh_history_*.mat']);
+        format_mh_history_file = 1;
     else
         error(['Estimation::load_mh_file: I cannot find any mh-history file in ' MetropolisFolder '!'])
     end
+else
+    format_mh_history_file = 0;
 end
 
-BaseName = [BaseName '_mh_history_'];
-
-% ... And read the last one.
-mh_history_info = load([BaseName num2str(length(mh_history_files)-1) '.mat']);
-
-% Solve version issues.
-if mh_history_info.version<2
-    mh_history_info.record.LastLogPost = mh_history_info.record.LastLogLiK;
-    mh_history_info.record.LastSeeds = mh_history_info.record.Seeds;
-    mh_history_info.record.InitialSeeds = NaN; % This information is forever lost...
-    mh_history_info.record = rmfield(mh_history_info.record,'LastLogLiK');
-    mh_history_info.record = rmfield(mh_history_info.record,'Seeds');
-    mh_history_info.version = 2;
-    record = mh_history_info.record;
-    version = mh_history_info.record;
-    save([BaseName '_mh_history_0.mat'],'record'); clear record;
-    save([BaseName '_mh_history_0.mat'],'version','-append'); clear version;
+if format_mh_history_file
+    load([BaseName '_mh_history.mat']);
+    record.LastLogPost = record.LastLogLiK;
+    record.LastSeeds = record.Seeds;
+    record.InitialSeeds = NaN; % This information is forever lost...
+    record = rmfield(record,'LastLogLiK');
+    record = rmfield(record,'Seeds');
+    save([BaseName '_mh_history_0.mat'],'record');
+else
+    load([BaseName '_mh_history_' num2str(length(mh_history_files)-1) '.mat']);
 end
 
 if isequal(nargout,0)
-    assignin('caller', 'record', mh_history_info.record);
+    assignin('caller', 'record', record);
 else
-    info = mh_history_info.record;
+    info = record;
 end
