dc3bcb47bf39afcc70c4ef18aafa6f8a4626b911
Stéphane Adjemian (Scylla) <stephane.adjemian@univ-lemans.fr>
2013-11-29 19:06:26 +0100

    Cosmetic (removed commented line of code).

--- matlab/utilities/tests/mtest.m
+++ matlab/utilities/tests/mtest.m
@@ -82,7 +82,6 @@ for i=1:nn
     fprintf(tid,'exception = lasterror;\n');
     if isoctave
         fprintf(tid, 'LOG = ''%s'';\n','The Log output is not available with Octave!');
-        %fprintf(fid,['LOG = sprintf(''LOG = Error in %%s (line %%d)\\n'', strvcat(exception.stack.name), strvcat(exception.stack.line));\n']);
     else
         fprintf(tid,['LOG = getReport(exception,''extended'');\n']);
     end
