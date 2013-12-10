albop/dynare

commit 3cb16af5e323ada152cbac0a2a55cd4607bf6e7b

Continue harmonizing figure titles in GSA

matlab/gsa/stab_map_.m
@@ -468,23 +468,23 @@ if pprior
     aunstablename=[aname, '_unst'];  aunstabletitle='Prior StabMap: Parameter driving explosiveness of solution';
     awronguniname=[aname, '_wrong']; awrongunititle='Prior StabMap: Parameter driving inability to find solution';
     % bivariate
-    auname='prior_unacceptable'; autitle='Prior Unacceptable';
-    aunstname='prior_unstable'; aunsttitle='Prior Unstable';
+    auname='prior_unacceptable'; autitle='Prior non-existence of unique stable solution (Unacceptable)';
+    aunstname='prior_unstable'; aunsttitle='Prior explosiveness of solution';
     aindname='prior_indeterm'; aindtitle='Prior Indeterminacy';
-    awrongname='prior_wrong'; awrongtitle='Prior No Solution Found';
-    asname='prior_stable'; astitle='Prior Stable';
+    awrongname='prior_wrong'; awrongtitle='Prior inability to find solution';
+    asname='prior_stable'; astitle='Prior unique Stable Saddle-Path';
 else
     % univariate
-    aname='mc_stab'; atitle='Posterior StabMap: Parameter driving non-existence of unique stable solution (Unacceptable)';
-    aindetname=[aname, '_indet']; aindettitle='Posterior StabMap: Parameter driving indeterminacy';
-    aunstablename=[aname, '_unst'];  aunstabletitle='Posterior StabMap: Parameter driving explosiveness of solution';
-    awronguniname=[aname, '_wrong']; awrongunititle='Posterior StabMap: Parameter driving inability to find solution';
+    aname='mc_stab'; atitle='MC (around posterior mode) StabMap: Parameter driving non-existence of unique stable solution (Unacceptable)';
+    aindetname=[aname, '_indet']; aindettitle='MC (around posterior mode) StabMap: Parameter driving indeterminacy';
+    aunstablename=[aname, '_unst'];  aunstabletitle='MC (around posterior mode) StabMap: Parameter driving explosiveness of solution';
+    awronguniname=[aname, '_wrong']; awrongunititle='MC (around posterior mode) StabMap: Parameter driving inability to find solution';
     % bivariate
-    auname='mc_unacceptable'; autitle='Posterior Unacceptable';
-    aunstname='mc_unstable'; aunsttitle='Posterior Unstable';
-    aindname='mc_indeterm';  aindtitle='Posterior Indeterminacy';
-    awrongname='mc_wrong'; awrongtitle='Posterior No Solution Found';
-    asname='mc_stable'; astitle='Posterior Stable';
+    auname='mc_unacceptable'; autitle='MC (around posterior mode) non-existence of unique stable solution (Unacceptable)';
+    aunstname='mc_unstable'; aunsttitle='MC (around posterior mode) explosiveness of solution';
+    aindname='mc_indeterm';  aindtitle='MC (around posterior mode) Indeterminacy';
+    awrongname='mc_wrong'; awrongtitle='MC (around posterior mode) inability to find solution';
+    asname='mc_stable'; astitle='MC (around posterior mode) Unique Stable Saddle-Path';
 end
 delete([OutputDirectoryName,filesep,fname_,'_',aname,'_*.*']);
 %delete([OutputDirectoryName,filesep,fname_,'_',aname,'_SA_*.*']);

matlab/gsa/stab_map_2.m
@@ -86,10 +86,14 @@ for j=1:npar,
                 fprintf(1,'%20s: corrcoef = %7.3f\n',tmp_name,c0(i2(jx),j));
                     
                 if ~options_.nograph,
-                    
+                if strcmp(fnam(1:2),'mc')
+                   type='MC (around posterior mode) StabMap: '; 
+                elseif strcmp(fnam(1:5),'prior')
+                   type='Prior StabMap: ';
+                end
                 if mod(j2,12)==1,
                     ifig=ifig+1;
-                    hh=dyn_figure(options_,'name',['Correlations in the ',figtitle,' sample ', num2str(ifig)]);
+                    hh=dyn_figure(options_,'name',[type,'Correlations in the ',figtitle,' sample ', num2str(ifig)]);
                 end
                 subplot(3,4,j2-(ifig-1)*12)
                 %             bar(c0(i2,j)),

commit f8ec220711c0bf173796d2188807386ba109a967

1) Fixed bug introduced in commit 3cb16af5e323ada152cbac0a2a55cd4607bf6e7b 2) Harmonize bivariate plots in the proper way 3) Harmonize bivariate plots for rmse analysis

matlab/gsa/filt_mc_.m
@@ -524,17 +524,17 @@ else
     % plot trade-offs
     if ~options_.nograph
     a00=jet(size(vvarvecm,1));
-    for ix=1:ceil(length(nsnam)/5),
-        if options_.opt_gsa.ppost
-            temp_name='RMSE Posterior Tradeoffs: Log Posterior';
+    if options_.opt_gsa.ppost
+        temp_name='RMSE Posterior Tradeoffs:';
+    else
+        if options_.opt_gsa.pprior
+            temp_name='RMSE Prior Tradeoffs:';
         else
-            if options_.opt_gsa.pprior
-                temp_name='RMSE Prior Tradeoffs: Log Posterior';
-            else
-                temp_name='RMSE MC Tradeoffs: Log Posterior';
-            end
-        end        
-        hh = dyn_figure(options_,'name',[temp_name,' ',int2str(ix)]);
+            temp_name='RMSE MC Tradeoffs;';
+        end
+    end
+    for ix=1:ceil(length(nsnam)/5),
+        hh = dyn_figure(options_,'name',[temp_name,' observed variables ',int2str(ix)]);
         for j=1+5*(ix-1):min(size(snam2,1),5*ix),
             subplot(2,3,j-5*(ix-1))
             %h0=cumplot(x(:,nsnam(j)+nshock));
@@ -617,7 +617,7 @@ else
                 fnam = ['rmse_mc_',deblank(vvarvecm(i,:))];
             end
         end
-        stab_map_2(x(ixx(1:nfilt0(i),i),:),alpha2,pvalue,fnam, OutDir);
+        stab_map_2(x(ixx(1:nfilt0(i),i),:),alpha2,pvalue,fnam, OutDir,[],[temp_name ' observed variable ' deblank(vvarvecm(i,:))]);
         
         %     [pc,latent,explained] = pcacov(c0);
         %     %figure, bar([explained cumsum(explained)])

matlab/gsa/stab_map_.m
@@ -471,11 +471,11 @@ if pprior
     aunstablename=[aname, '_unst'];  aunstabletitle='Prior StabMap: Parameter driving explosiveness of solution';
     awronguniname=[aname, '_wrong']; awrongunititle='Prior StabMap: Parameter driving inability to find solution';
     % bivariate
-    auname='prior_unacceptable'; autitle='Prior non-existence of unique stable solution (Unacceptable)';
-    aunstname='prior_unstable'; aunsttitle='Prior explosiveness of solution';
-    aindname='prior_indeterm'; aindtitle='Prior Indeterminacy';
-    awrongname='prior_wrong'; awrongtitle='Prior inability to find solution';
-    asname='prior_stable'; astitle='Prior unique Stable Saddle-Path';
+    auname='prior_unacceptable'; autitle='Prior StabMap: non-existence of unique stable solution (Unacceptable)';
+    aunstname='prior_unstable'; aunsttitle='Prior StabMap: explosiveness of solution';
+    aindname='prior_indeterm'; aindtitle='Prior StabMap: Indeterminacy';
+    awrongname='prior_wrong'; awrongtitle='Prior StabMap: inability to find solution';
+    asname='prior_stable'; astitle='Prior StabMap: unique Stable Saddle-Path';
 else
     % univariate
     aname='mc_stab'; atitle='MC (around posterior mode) StabMap: Parameter driving non-existence of unique stable solution (Unacceptable)';
@@ -483,11 +483,11 @@ else
     aunstablename=[aname, '_unst'];  aunstabletitle='MC (around posterior mode) StabMap: Parameter driving explosiveness of solution';
     awronguniname=[aname, '_wrong']; awrongunititle='MC (around posterior mode) StabMap: Parameter driving inability to find solution';
     % bivariate
-    auname='mc_unacceptable'; autitle='MC (around posterior mode) non-existence of unique stable solution (Unacceptable)';
-    aunstname='mc_unstable'; aunsttitle='MC (around posterior mode) explosiveness of solution';
-    aindname='mc_indeterm';  aindtitle='MC (around posterior mode) Indeterminacy';
-    awrongname='mc_wrong'; awrongtitle='MC (around posterior mode) inability to find solution';
-    asname='mc_stable'; astitle='MC (around posterior mode) Unique Stable Saddle-Path';
+    auname='mc_unacceptable'; autitle='MC (around posterior mode) StabMap: non-existence of unique stable solution (Unacceptable)';
+    aunstname='mc_unstable'; aunsttitle='MC (around posterior mode) StabMap: explosiveness of solution';
+    aindname='mc_indeterm';  aindtitle='MC (around posterior mode) StabMap: Indeterminacy';
+    awrongname='mc_wrong'; awrongtitle='MC (around posterior mode) StabMap: inability to find solution';
+    asname='mc_stable'; astitle='MC (around posterior mode) StabMap: Unique Stable Saddle-Path';
 end
 delete([OutputDirectoryName,filesep,fname_,'_',aname,'_*.*']);
 %delete([OutputDirectoryName,filesep,fname_,'_',aname,'_SA_*.*']);

matlab/gsa/stab_map_2.m
@@ -86,14 +86,9 @@ for j=1:npar,
                 fprintf(1,'%20s: corrcoef = %7.3f\n',tmp_name,c0(i2(jx),j));
                     
                 if ~options_.nograph,
-                if strcmp(fnam(1:2),'mc')
-                   type='MC (around posterior mode) StabMap: '; 
-                elseif strcmp(fnam(1:5),'prior')
-                   type='Prior StabMap: ';
-                end
                 if mod(j2,12)==1,
                     ifig=ifig+1;
-                    hh=dyn_figure(options_,'name',[type,'Correlations in the ',figtitle,' sample ', num2str(ifig)]);
+                    hh=dyn_figure(options_,'name',[figtitle,' sample bivariate projection ', num2str(ifig)]);
                 end
                 subplot(3,4,j2-(ifig-1)*12)
                 %             bar(c0(i2,j)),