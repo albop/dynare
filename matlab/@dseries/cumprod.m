fa6e97a92989c458b7bbc4cb6acbb96c93d34907
Stéphane Adjemian (Scylla) <stephane.adjemian@univ-lemans.fr>
2014-01-29 17:22:44 +0100

    Fixed copyright year.

--- matlab/@dseries/cumprod.m
+++ matlab/@dseries/cumprod.m
@@ -10,7 +10,7 @@ function B = cumprod(varargin) % --*-- Unitary tests --*--
 % OUTPUTS
 %  o B     dseries object.
 
-% Copyright (C) 2013 Dynare Team
+% Copyright (C) 2014 Dynare Team
 %
 % This file is part of Dynare.
 %
