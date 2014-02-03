albop/dynare

commit 4caa36232b647da91710af34f7d10786415ef776

reporting: fix assertion

matlab/reports/@report_series/getLine.m
@@ -50,8 +50,8 @@ assert(isempty(o.graphMarker) || any(strcmp(o.graphMarker, valid_graphMarker)), 
 
 assert(ischar(o.graphMarkerEdgeColor), '@report_series.getLine: graphMarkerEdgeColor must be a string');
 assert(ischar(o.graphMarkerFaceColor), '@report_series.getLine: graphMarkerFaceColor must be a string');
-assert(isfloat(o.graphMarkerSize), ['@report_series.getLine: graphMarkerSize must be a ' ...
-                    'positive number']);
+assert(isfloat(o.graphMarkerSize) && o.graphLineWidth > 0, ...
+                    '@report_series.getLine: graphMarkerSize must be a positive number');
 
 % Marker & Line
 assert(~(strcmp(o.graphLineStyle, 'none') && isempty(o.graphMarker)), ['@report_series.getLine: ' ...

commit 4dd462e2bb488e8d07278e7316162ab12d975b09

fix bug introduced in 4caa36232b647da91710af34f7d10786415ef776

matlab/reports/@report_series/getLine.m
@@ -50,7 +50,7 @@ assert(isempty(o.graphMarker) || any(strcmp(o.graphMarker, valid_graphMarker)), 
 
 assert(ischar(o.graphMarkerEdgeColor), '@report_series.getLine: graphMarkerEdgeColor must be a string');
 assert(ischar(o.graphMarkerFaceColor), '@report_series.getLine: graphMarkerFaceColor must be a string');
-assert(isfloat(o.graphMarkerSize) && o.graphLineWidth > 0, ...
+assert(isfloat(o.graphMarkerSize) && o.graphMarkerSize > 0, ...
                     '@report_series.getLine: graphMarkerSize must be a positive number');
 
 % Marker & Line