albop/dynare

commit bb18c9386d41c365cedbb6784f854c53638541c0

Fixed bug.
MCMC convergence diagnostics should be computed even if mh_replic is less than 2000, provided that the total number of iterations is greater than 2000 (if option load_mh_file is used).

matlab/McMCDiagnostics.m
@@ -69,6 +69,15 @@ if issue_an_error_message
     error('Estimation::mcmc::diagnostics: I cannot proceed because some MCMC files are missing. Check your MCMC files...')
 end
 
+PastDraws = sum(record.MhDraws,1);
+LastFileNumber = PastDraws(2);
+LastLineNumber = record.MhDraws(end,3);
+NumberOfDraws  = PastDraws(1);
+
+if LastLineNumber<=2000
+    warning(['estimation:: MCMC convergence diagnostics are not computed because the total number of iterations is less than 2000!'])
+    return
+end
 
 if nblck == 1 % Brooks and Gelman tests need more than one block
     convergence_diagnostics_geweke=zeros(npar,4+2*length(options_.convergence.geweke.taper_steps));
@@ -120,11 +129,6 @@ if nblck == 1 % Brooks and Gelman tests need more than one block
     skipline(2);
     return;
 end
-
-PastDraws = sum(record.MhDraws,1);
-LastFileNumber = PastDraws(2);
-LastLineNumber = record.MhDraws(end,3);
-NumberOfDraws  = PastDraws(1);
 
 Origin = 1000;
 StepSize = ceil((NumberOfDraws-Origin)/100);% So that the computational time does not 

matlab/dynare_estimation_1.m
@@ -789,7 +789,7 @@ if (any(bayestopt_.pshape  >0 ) && options_.mh_replic) || ...
         CutSample(M_, options_, estim_params_);
         return
     else
-        if ~options_.nodiagnostic && options_.mh_replic > 2000
+        if ~options_.nodiagnostic && options_.mh_replic>0
             oo_= McMCDiagnostics(options_, estim_params_, M_,oo_);
         end
         %% Here i discard first half of the draws:

commit 78a015a7d2d9ba0c2b666f9c0dea66dcd82131d2

Fixed bug introduced in commit bb18c9386d41c365cedbb6784f854c53638541c0.

matlab/McMCDiagnostics.m
@@ -74,7 +74,7 @@ LastFileNumber = PastDraws(2);
 LastLineNumber = record.MhDraws(end,3);
 NumberOfDraws  = PastDraws(1);
 
-if LastLineNumber<=2000
+if NumberOfDraws<=2000
     warning(['estimation:: MCMC convergence diagnostics are not computed because the total number of iterations is less than 2000!'])
     return
 end