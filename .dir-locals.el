c24ae80ae26484cbbf4349399f1d01553adfa12b
Sébastien Villemot <sebastien@dynare.org>
2014-04-01 17:31:50 +0200

    Add per-directory local Emacs variables.
    The idea is to enforce the coding style for Emacs users.

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil .
+      ((c-file-style . "gnu")
+       (indent-tabs-mode . nil)
+       (fill-column . 79)))
+ (makefile-mode . ((indent-tabs-mode . t)))
+ (matlab-mode . ((matlab-indent-level . 4)
+                 (matlab-indent-function-body . nil))))
