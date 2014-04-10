albop/dynare

commit 0e204673fd53727e591f4a43387feed8afa4f40d

Correctly set oo_.deterministic_simulation.status in sim1_purely_{backward,forward}.m.

matlab/sim1_purely_backward.m
@@ -1,7 +1,7 @@
 function sim1_purely_backward()
 % Performs deterministic simulation of a purely backward model
 
-% Copyright (C) 2012-2013 Dynare Team
+% Copyright (C) 2012-2014 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -36,15 +36,22 @@ function sim1_purely_backward()
     
     model_dynamic = str2func([M_.fname,'_dynamic']);
 
+    oo_.deterministic_simulation.status = 1;
+
     for it = 2:options_.periods+1
         yb = oo_.endo_simul(:,it-1); % Values at previous period, also used as guess value for current period
         yb1 = yb(iyb);
        
-        tmp = solve1(model_dynamic, [yb1; yb], 1:M_.endo_nbr, nyb+1:nyb+ ...
-                     M_.endo_nbr, 1, options_.gstep, ...
-                     options_.solve_tolf,options_.solve_tolx, ...
-                     options_.simul.maxit,options_.debug,oo_.exo_simul, ...
-                     M_.params, oo_.steady_state, it);
+        [tmp, check] = solve1(model_dynamic, [yb1; yb], 1:M_.endo_nbr, nyb+1:nyb+ ...
+                              M_.endo_nbr, 1, options_.gstep, ...
+                              options_.solve_tolf,options_.solve_tolx, ...
+                              options_.simul.maxit,options_.debug,oo_.exo_simul, ...
+                              M_.params, oo_.steady_state, it);
+
+        if info
+            oo_.deterministic_simulation.status = 0;
+        end
+
         oo_.endo_simul(:,it) = tmp(nyb+1:nyb+M_.endo_nbr);
     end
     

matlab/sim1_purely_forward.m
@@ -1,7 +1,7 @@
 function sim1_purely_forward()
 % Performs deterministic simulation of a purely forward model
 
-% Copyright (C) 2012 Dynare Team
+% Copyright (C) 2012-2014 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -29,15 +29,22 @@ function sim1_purely_forward()
     
     model_dynamic = str2func([M_.fname,'_dynamic']);
 
+    oo_.deterministic_simulation.status = 1;
+
     for it = options_.periods:-1:1
         yf = oo_.endo_simul(:,it+1); % Values at next period, also used as guess value for current period
         yf1 = yf(iyf);
        
-        tmp = solve1(model_dynamic, [yf; yf1], 1:M_.endo_nbr, 1:M_.endo_nbr, ...
-                     1, options_.gstep, options_.solve_tolf, ...
-                     options_.solve_tolx, options_.simul.maxit, ...
-                     options_.debug,oo_.exo_simul, M_.params, oo_.steady_state, ...
-                     it);
+        [tmp, info] = solve1(model_dynamic, [yf; yf1], 1:M_.endo_nbr, 1:M_.endo_nbr, ...
+                             1, options_.gstep, options_.solve_tolf, ...
+                             options_.solve_tolx, options_.simul.maxit, ...
+                             options_.debug,oo_.exo_simul, M_.params, oo_.steady_state, ...
+                             it);
+
+        if info
+            oo_.deterministic_simulation.status = 0;
+        end
+
         oo_.endo_simul(:,it) = tmp(1:M_.endo_nbr);
     end
     

commit 7b882b2f60e4b2e85075034cf846c80a6c28d5d2

Fix bug introduced in 0e204673.

matlab/sim1_purely_backward.m
@@ -48,7 +48,7 @@ function sim1_purely_backward()
                               options_.simul.maxit,options_.debug,oo_.exo_simul, ...
                               M_.params, oo_.steady_state, it);
 
-        if info
+        if check
             oo_.deterministic_simulation.status = 0;
         end
 