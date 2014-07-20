3c9b031bd1876e1fc317510845849abb4733d4b9
Johannes Pfeifer <jpfeifer@gmx,de>
2014-07-20 12:46:14 +0200

    Fix bug in dynare_estimation_init.m if steady state does not solve
    There was no check in dynare_estimation_init.m whether the steady state solved, but the noconstant option was set nevertheless. This gave rise to cases where a steady state file returned 0 for the observables, but the steady state did not exits. Due to later using use_calibration, this problem with the initial values was not detected, but the observation equation featured no constant although it should. The solution is to move the use_calibration option before the first steady state computation and then issue an error if the parameterization does not work

--- matlab/dynare_estimation_1.m
+++ matlab/dynare_estimation_1.m
@@ -107,29 +107,6 @@ if options_.dsge_var
     end
 end
 
-%check for calibrated covariances before updating parameters
-if ~isempty(estim_params_)
-    estim_params_=check_for_calibrated_covariances(xparam1,estim_params_,M_);
-end
-
-%%read out calibration that was set in mod-file and can be used for initialization
-xparam1_calib=get_all_parameters(estim_params_,M_); %get calibrated parameters
-if ~any(isnan(xparam1_calib)) %all estimated parameters are calibrated
-    full_calibration_detected=1;
-else
-    full_calibration_detected=0;
-end
-if options_.use_calibration_initialization %set calibration as starting values
-    [xparam1,estim_params_]=do_parameter_initialization(estim_params_,xparam1_calib,xparam1);   %get explicitly initialized parameters that have precedence to calibrated values
-    try
-        check_prior_bounds(xparam1,bounds,M_,estim_params_,options_,bayestopt_); %check whether calibration satisfies prior bounds
-    catch
-        e = lasterror();
-        fprintf('Cannot use parameter values from calibration as they violate the prior bounds.')
-        rethrow(e);
-    end
-end
-
 % Set sigma_e_is_diagonal flag (needed if the shocks block is not declared in the mod file).
 M_.sigma_e_is_diagonal = 1;
 if estim_params_.ncx || any(nnz(tril(M_.Correlation_matrix,-1))) || isfield(estim_params_,'calibrated_covariances')
@@ -180,7 +157,7 @@ try
     oo_ = initial_estimation_checks(objective_function,xparam1,dataset_,dataset_info,M_,estim_params_,options_,bayestopt_,bounds,oo_);
 catch % if check fails, provide info on using calibration if present
     e = lasterror();
-    if full_calibration_detected %calibrated model present and no explicit starting values
+    if estim_params_.full_calibration_detected %calibrated model present and no explicit starting values
         skipline(1);
         fprintf('ESTIMATION_CHECKS: There was an error in computing the likelihood for initial parameter values.\n')
         fprintf('ESTIMATION_CHECKS: You should try using the calibrated version of the model as starting values. To do\n')
