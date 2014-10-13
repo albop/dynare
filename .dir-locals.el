bc1243f5019a481d368f5335f5923e0e85cc1d16
Stéphane Adjemian (Karaba) <stephane.adjemian@univ-lemans.fr>
2014-10-13 12:52:51 +0200

    Removed config for matlab files in .dir-locals.el (for emacs users).

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,5 +3,3 @@
        (indent-tabs-mode . nil)
        (fill-column . 79)))
  (makefile-mode . ((indent-tabs-mode . t)))
- (matlab-mode . ((matlab-indent-level . 4)
-                 (matlab-indent-function-body . nil))))
