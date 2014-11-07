ff3c8e94e194b704b03b4a6c39233d0908bcaf4d
Johannes Pfeifer <jpfeifer@gmx,de>
2014-11-07 12:18:05 +0100

    Clean up hessian.m
    Fix description of algorithm and document the function better. Also removes manual increasing of loop variable and moves computation of temporary variable not depending on loop variables outside of the loop as it needs to be computed only once.

--- matlab/hessian.m
+++ matlab/hessian.m
@@ -12,13 +12,21 @@ function hessian_mat = hessian(func,x,gstep,varargin)
 %    hessian_mat [double]   Hessian matrix
 %
 % ALGORITHM
-%    Uses Abramowitz and Stegun (1965) formulas 25.3.24 and 25.3.27 p. 884
+%    Uses Abramowitz and Stegun (1965) formulas 25.3.23 
+% \[
+%     \frac{\partial^2 f_{0,0}}{\partial {x^2}} = \frac{1}{h^2}\left( f_{1,0} - 2f_{0,0} + f_{ - 1,0} \right)
+% \]
+% and 25.3.27 p. 884
+% 
+% \[
+%     \frac{\partial ^2f_{0,0}}{\partial x\partial y} = \frac{-1}{2h^2}\left(f_{1,0} + f_{-1,0} + f_{0,1} + f_{0,-1} - 2f_{0,0} - f_{1,1} - f_{-1,-1} \right)
+% \]
 %
 % SPECIAL REQUIREMENTS
 %    none
 %  
 
-% Copyright (C) 2001-2012 Dynare Team
+% Copyright (C) 2001-2014 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -49,34 +57,36 @@ xh1=x;
 f0=feval(func,x,varargin{:});
 f1=zeros(size(f0,1),n);
 f_1=f1;
-for i=1:n    
+for i=1:n
+    %do step up
     xh1(i)=x(i)+h1(i);
     f1(:,i)=feval(func,xh1,varargin{:});
+    %do step up
     xh1(i)=x(i)-h_1(i);
     f_1(:,i)=feval(func,xh1,varargin{:});
-    xh1(i)=x(i);
-    i=i+1;
+    xh1(i)=x(i);%reset parameter
 end
 xh_1=xh1;
 hessian_mat = zeros(size(f0,1),n*n);
+temp=f1+f_1-f0*ones(1,n); %term f_(1,0)+f_(-1,0)-f_(0,0) used later
 for i=1:n    
-    if i > 1        
+    if i > 1  %fill symmetric part of Hessian based on previously computed results      
         k=[i:n:n*(i-1)];
         hessian_mat(:,(i-1)*n+1:(i-1)*n+i-1)=hessian_mat(:,k);
     end     
-    hessian_mat(:,(i-1)*n+i)=(f1(:,i)+f_1(:,i)-2*f0)./(h1(i)*h_1(i));
-    temp=f1+f_1-f0*ones(1,n);
+    hessian_mat(:,(i-1)*n+i)=(f1(:,i)+f_1(:,i)-2*f0)./(h1(i)*h_1(i)); %formula 25.3.23
     for j=i+1:n        
+        %step in up direction
         xh1(i)=x(i)+h1(i);
         xh1(j)=x(j)+h_1(j);
+        %step in down direction
         xh_1(i)=x(i)-h1(i);
         xh_1(j)=x(j)-h_1(j);
-        hessian_mat(:,(i-1)*n+j)=-(-feval(func,xh1,varargin{:})-feval(func,xh_1,varargin{:})+temp(:,i)+temp(:,j))./(2*h1(i)*h_1(j));
+        hessian_mat(:,(i-1)*n+j)=-(-feval(func,xh1,varargin{:})-feval(func,xh_1,varargin{:})+temp(:,i)+temp(:,j))./(2*h1(i)*h_1(j)); %formula 25.3.27
+        %reset grid points
         xh1(i)=x(i);
         xh1(j)=x(j);
         xh_1(i)=x(i);
         xh_1(j)=x(j);
-        j=j+1;
     end    
-    i=i+1;
 end
