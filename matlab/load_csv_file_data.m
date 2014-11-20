a2bd920d6f098b6a80be1f75d60cbd22c42899f5
Houtan Bastani <houtan@dynare.org>
2014-11-20 10:35:59 +0100

    Workaround for #755 (cherry picked from commit 9a3cca2a55d57c3148fbcfd892a67a1d9731e9b1)
    Conflicts:
    	matlab/load_csv_file_data.m

--- matlab/load_csv_file_data.m
+++ matlab/load_csv_file_data.m
@@ -1,5 +1,5 @@
 function [freq, init, data, varlist] = load_csv_file_data(file)
-
+%function [freq, init, data, varlist] = load_csv_file_data(file)
 % Loads data in a csv file.
 %
 % INPUTS 
@@ -16,7 +16,7 @@ function [freq, init, data, varlist] = load_csv_file_data(file)
 %  names. Similarly, if the first column does not contain dates, then
 %  freq will be 1 and init will be year 1.
 
-% Copyright (C) 2012-2013 Dynare Team
+% Copyright (C) 2012-2014 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -41,26 +41,114 @@ varlist = [];
 assert(exist(file, 'file') == 2, ['load_csv_file_data: I can''t find file ' file '!']);
 
 if isoctave
-    if ~user_has_octave_forge_package('io')
-        try
-            pkg load io
-        catch
-            error(['The io package is required to read CSV files from Octave. ' ...
-                'It can be installed by running the following from the Octave ' ...
-                ' command line: pkg install -forge io']);
+    fid = fopen(file, 'r');
+    firstline = fgetl(fid, 4097);
+    fclose(fid);
+    if length(firstline) < 4097
+        if ~user_has_octave_forge_package('io')
+            try
+                pkg load io
+            catch
+                error(['The io package is required to read CSV files from Octave. ' ...
+                       'It can be installed by running the following from the Octave ' ...
+                       ' command line: pkg install -forge io']);
+            end
         end
+    else
+        fid = fopen(file, 'r');
+        bfile = fread(fid);
+        fclose(fid);
+        
+        if isunix || ismac
+            newline_code = 10;
+        elseif ispc
+            newline_code = 13;
+        else
+            error('load_csv_file_data is not implemented for your OS');
+        end
+        
+        % Get the positions of the end-of-line code
+        end_of_line_locations = find(bfile==newline_code);
+        if ispc && isempty(end_of_line_locations)
+            newline_code=10;
+            end_of_line_locations = find(bfile==newline_code);
+        end;
+        tmp = find(bfile==newline_code);
+        
+        % Get the number of lines in the file
+        ndx = length(tmp);
+        
+        % Create a cell of indices for each line
+        b = [1; end_of_line_locations+1];
+        c = [end_of_line_locations-1; length(bfile)+1];
+        b = b(1:end-1);
+        c = c(1:end-1);
+        linea = 1;
+        if withnames
+            % Get the first line of the csv file (names of the variables).
+            linee = char(transpose(bfile(b(linea):c(linea))));
+            % Get the content of the first line and determine the number of variables and their names.
+            [B,C] = get_cells_id(linee,',');
+            if withtime
+                B = B(2:end);
+                C = C(2:end);
+            end
+            varlist = cell(length(B),1);
+            number_of_variables = length(varlist);
+            for i=1:number_of_variables
+                varlist(i) = {linee(B(i):C(i))};
+            end
+            varlist = strtrim(varlist);
+            linea = linea+1;
+        end
+        
+        % Get following line (number 1 or 2 depending on withnames flag)
+        linee = char(transpose(bfile(b(linea):c(linea))));
+        comma_locations = transpose(strfind(linee,','));
+        B = 1;
+        C = comma_locations(1)-1;
+        if withtime
+            tmp = linee(B:C);
+            % Check the dates formatting
+            if isnumeric(tmp) && isint(tmp)
+                tmp = [num2str(tmp) 'Y'];
+            end
+            if ~isdate(tmp)
+                error('load_csv_file_data:: Formatting error. I can''t read the dates!')
+            end
+            init = dates(tmp);
+            freq = init.freq;
+            first = 2;
+        else
+            first = 1;
+        end
+        
+        if ~withnames
+            number_of_variables = length(tmp)-withtime;
+        end
+        
+        % Initialization of matrix data.
+        data = zeros(ndx,number_of_variables);
+        
+        % Populate data.
+        for linea = 1+withnames:ndx
+            linee = char(transpose(bfile(b(linea):c(linea))));
+            [B,C] = get_cells_id(linee,',');
+            for i=first:length(B)
+                data(linea,i-withtime) = str2double(linee(B(i):C(i)));
+            end
+        end
+        
+        % Remove first line if withnames
+        data = data(1+withnames:ndx, :);
     end
-    A = csv2cell(file);
-    [data, T, L] = parsecell(A);
-    withvars = L.numlimits(2,1) > L.txtlimits(2,1);
-    withtime = L.numlimits(1,1) > L.txtlimits(1,1);
 else
     A = importdata(file, ',');
     if ~isstruct(A)
         data = A;
         T = {};
         withvars = 0;
-        withtime = 0;
+            withtime = 0;
     else
         data = A.data;
         T = A.textdata;
