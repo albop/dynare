f1d70605201d6d1b104ebbcfba71134a8303f80d
Stéphane Adjemian (Scylla) <stephane.adjemian@univ-lemans.fr>
2015-03-13 17:25:06 +0100

    Added explicit extension for generated mat files (needed by octave).

--- tests/initval_file/ramst_initval_file_data.m
+++ tests/initval_file/ramst_initval_file_data.m
@@ -1,7 +1,7 @@
 x = vertcat([ 1; 1.2 ], repmat(1, 200, 1));
 k = repmat(12.7551, 202, 1);
 c = repmat(1.53061, 202, 1);
-save('ramst_initval_file_data_col_vec_mat');
+save('ramst_initval_file_data_col_vec_mat.mat');
 
 if ispc()
     xlswrite('ramst_initval_file_excel',[x k c],1,'A2');
@@ -11,4 +11,4 @@ end
 c=c';
 k=k';
 x=x';
-save('ramst_initval_file_data_row_vec_mat');
+save('ramst_initval_file_data_row_vec_mat.mat');
