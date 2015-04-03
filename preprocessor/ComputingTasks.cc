28f9e3ed09b131e124bd6101fa9b3bd94a001507
Houtan Bastani <houtan@dynare.org>
2015-04-03 16:46:44 +0200

    preprocessor: take care of extra exos in osr

--- preprocessor/ComputingTasks.cc
+++ preprocessor/ComputingTasks.cc
@@ -1295,6 +1295,29 @@ OsrStatement::checkPass(ModFileStructure &mod_file_struct, WarningConsolidation 
     mod_file_struct.k_order_solver = true;
 }
 
+Statement *
+OsrStatement::cloneAndReindexSymbIds(DataTree &dynamic_datatree, SymbolTable &orig_symbol_table)
+{
+  SymbolList new_symbol_list;
+  try
+    {
+      SymbolTable *new_symbol_table =  dynamic_datatree.getSymbolTable();
+      vector<string> symbols = symbol_list.get_symbols();
+      for (vector<string>::const_iterator it = symbols.begin(); it != symbols.end(); it++)
+        {
+          new_symbol_table->getID(*it);
+          new_symbol_list.addSymbol(*it);
+        }
+    }
+  catch (SymbolTable::UnknownSymbolNameException &e)
+    {
+      cerr << "ERROR: A variable in the osr statement was not found in the symbol table" << endl
+           << "       This likely means that you have declared a varexo that is not used in the model" << endl;
+      exit(EXIT_FAILURE);
+    }
+  return new OsrStatement(new_symbol_list, options_list);
+}
+
 void
 OsrStatement::writeOutput(ostream &output, const string &basename) const
 {
