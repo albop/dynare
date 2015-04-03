albop/dynare

commit 363c4a61222103fda22adff9bdb1115a6073428a

Use local variables for restrict var list, to avoid overwriting the global and messing likelihood evaluations later on.

matlab/DsgeSmoother.m
@@ -69,9 +69,10 @@ M_ = set_all_parameters(xparam1,estim_params_,M_);
 %------------------------------------------------------------------------------
 % 2. call model setup & reduction program
 %------------------------------------------------------------------------------
-oo_.dr.restrict_var_list = bayestopt_.smoother_var_list;
-oo_.dr.restrict_columns = bayestopt_.smoother_restrict_columns;
-[T,R,SteadyState,info,M_,options_,oo_] = dynare_resolve(M_,options_,oo_);
+DynareResults = oo_;
+DynareResults.dr.restrict_var_list = bayestopt_.smoother_var_list;
+DynareResults.dr.restrict_columns = bayestopt_.smoother_restrict_columns;
+[T,R,SteadyState,info,M_,options_,DynareResults] = dynare_resolve(M_,options_,DynareResults);
 bayestopt_.mf = bayestopt_.smoother_mf;
 if options_.noconstant
     constant = zeros(vobs,1);

commit 6aa5ceabb2de37ded2315fa9b143203170fbf403

Fixed bug introduced in 363c4a61222103fda22adff9bdb1115a6073428a.

matlab/DsgeSmoother.m
@@ -69,10 +69,16 @@ M_ = set_all_parameters(xparam1,estim_params_,M_);
 %------------------------------------------------------------------------------
 % 2. call model setup & reduction program
 %------------------------------------------------------------------------------
-DynareResults = oo_;
-DynareResults.dr.restrict_var_list = bayestopt_.smoother_var_list;
-DynareResults.dr.restrict_columns = bayestopt_.smoother_restrict_columns;
-[T,R,SteadyState,info,M_,options_,DynareResults] = dynare_resolve(M_,options_,DynareResults);
+oldoo.restrict_var_list = oo_.dr.restrict_var_list; 
+oldoo.restrict_columns = oo_.dr.restrict_columns;
+oo_.dr.restrict_var_list = bayestopt_.smoother_var_list;
+oo_.dr.restrict_columns = bayestopt_.smoother_restrict_columns;
+
+[T,R,SteadyState,info,M_,options_,oo_] = dynare_resolve(M_,options_,oo_);
+
+oo_.dr.restrict_var_list = oldoo.restrict_var_list;
+oo_.dr.restrict_columns = oldoo.restrict_columns;
+
 bayestopt_.mf = bayestopt_.smoother_mf;
 if options_.noconstant
     constant = zeros(vobs,1);