a7ffb0e6c84567cab4c8f107533c3e07e208c59e
Houtan Bastani <houtan@dynare.org>
2015-04-14 14:42:17 +0200

    preprocessor: take care of exos in calib_smoother statements

--- preprocessor/ComputingTasks.cc
+++ preprocessor/ComputingTasks.cc
@@ -3687,6 +3687,29 @@ CalibSmootherStatement::checkPass(ModFileStructure &mod_file_struct, WarningCons
   mod_file_struct.calib_smoother_present = true;
 }
 
+Statement *
+CalibSmootherStatement::cloneAndReindexSymbIds(DataTree &dynamic_datatree, SymbolTable &orig_symbol_table)
+{
+  SymbolList new_symbol_list;
+  try
+    {
+      SymbolTable *new_symbol_table =  dynamic_datatree.getSymbolTable();
+      vector<string> symbols = symbol_list.get_symbols();
+      for (vector<string>::const_iterator it = symbols.begin(); it != symbols.end(); it++)
+        {
+          new_symbol_table->getID(*it);
+          new_symbol_list.addSymbol(*it);
+        }
+    }
+  catch (...)
+    {
+      cerr << "ERROR: A variable in the calib_smoother statement was not found in the symbol table" << endl
+           << "       This likely means that you have declared a varexo that is not used in the model" << endl;
+      exit(EXIT_FAILURE);
+    }
+  return new CalibSmootherStatement(new_symbol_list, options_list);
+}
+
 void
 CalibSmootherStatement::writeOutput(ostream &output, const string &basename) const
 {
