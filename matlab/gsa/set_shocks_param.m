albop/dynare

commit 3ea44d47cf04caf3c69ae482eb5d2a365deb5c3c

Port changes from #511 to gsa/set_shocks_param.m
Closes #662

matlab/gsa/set_shocks_param.m
@@ -1,6 +1,8 @@
 function set_shocks_param(xparam1)
+% function set_shocks_param(xparam1)
+% Set the structural and measurement error variances and covariances
 
-% Copyright (C) 2012 Dynare Team
+% Copyright (C) 2012-2015 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -18,31 +20,86 @@ function set_shocks_param(xparam1)
 % along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
 
 global estim_params_ M_
-  
-  nvx = estim_params_.nvx;
-  ncx = estim_params_.ncx;
-  np = estim_params_.np;
-  Sigma_e = M_.Sigma_e;
-  offset = 0;
-  if nvx
-    offset = offset + nvx;
+
+nvx = estim_params_.nvx;
+ncx = estim_params_.ncx;
+nvn = estim_params_.nvn;
+ncn = estim_params_.ncn;
+
+Sigma_e = M_.Sigma_e;
+Correlation_matrix = M_.Correlation_matrix;
+
+H = M_.H;
+Correlation_matrix_ME = M_.Correlation_matrix_ME;
+% setting shocks variance on the diagonal of Covariance matrix; used later
+% for updating covariances
+if nvx
     var_exo = estim_params_.var_exo;
     for i=1:nvx
-      k = var_exo(i,1);
-      Sigma_e(k,k) = xparam1(i)^2;
+        k =var_exo(i,1);
+        Sigma_e(k,k) = xparam1(i)^2;
     end
-  end
-  
-  if ncx
-    offset = offset + estim_params_.nvn;
+end
+% update offset
+offset = nvx;
+
+% setting measument error variance; on the diagonal of Covariance matrix; used later
+% for updating covariances
+if nvn
+    for i=1:nvn
+        k = estim_params_.nvn_observable_correspondence(i,1);
+        H(k,k) = xparam1(i+offset)^2;
+    end
+end
+
+% update offset
+offset = nvx+nvn;
+
+% setting shocks covariances
+if ncx
     corrx = estim_params_.corrx;
     for i=1:ncx
-      k1 = corrx(i,1);
-      k2 = corrx(i,2);
-      Sigma_e(k1,k2) = xparam1(i+offset)*sqrt(Sigma_e_(k1,k1)*Sigma_e_(k2,k2));
-      Sigma_e(k2,k1) = Sigma_e_(k1,k2);
+        k1 = corrx(i,1);
+        k2 = corrx(i,2);
+        Correlation_matrix(k1,k2) = xparam1(i+offset);
+        Correlation_matrix(k2,k1) = Correlation_matrix(k1,k2);
     end
-  end
+end
+%build covariance matrix from correlation matrix and variances already on
+%diagonal
+Sigma_e = diag(sqrt(diag(Sigma_e)))*Correlation_matrix*diag(sqrt(diag(Sigma_e))); 
+%if calibrated covariances, set them now to their stored value
+if isfield(estim_params,'calibrated_covariances')
+    Sigma_e(estim_params_.calibrated_covariances.position)=estim_params_.calibrated_covariances.cov_value;
+end
+% update offset
+offset = nvx+nvn+ncx;
+
+% setting measurement error covariances
+if ncn
+    corrn_observable_correspondence = estim_params_.corrn_observable_correspondence;
+    for i=1:ncn
+        k1 = corrn_observable_correspondence(i,1);
+        k2 = corrn_observable_correspondence(i,2);
+        Correlation_matrix_ME(k1,k2) = xparam1(i+offset);
+        Correlation_matrix_ME(k2,k1) = Correlation_matrix_ME(k1,k2);
+    end
+end
+%build covariance matrix from correlation matrix and variances already on
+%diagonal
+H = diag(sqrt(diag(H)))*Correlation_matrix_ME*diag(sqrt(diag(H)));
+%if calibrated covariances, set them now to their stored value
+if isfield(estim_params,'calibrated_covariances_ME')
+    H(estim_params_.calibrated_covariances_ME.position)=estim_params_.calibrated_covariances_ME.cov_value;
+end
+
   
-  
-  M_.Sigma_e = Sigma_e;
+% updating matrices in M
+if nvx || ncx
+    M_.Sigma_e = Sigma_e;
+    M_.Correlation_matrix=Correlation_matrix;
+end
+if nvn || ncn
+    M_.H = H;
+    M_.Correlation_matrix_ME=Correlation_matrix_ME;    
+end 

matlab/set_all_parameters.m
@@ -32,8 +32,12 @@ function M = set_all_parameters(xparam1,estim_params,M)
 %! @sp 2
 %! @end deftypefn
 %@eod:
+%
+% Remarks: Changes to this file also need to be ported to
+% gsa/set_shocks_param.m
 
-% Copyright (C) 2003-2013 Dynare Team
+
+% Copyright (C) 2003-2015 Dynare Team
 %
 % This file is part of Dynare.
 %

commit 88ae542a8a1fea7a1e903af62c60453842c730ef

Fix copy and paste error in set_shocks_param.m introduced in 3ea44d47cf04caf3c69ae482eb5d2a365deb5c3c

matlab/gsa/set_shocks_param.m
@@ -69,7 +69,7 @@ end
 %diagonal
 Sigma_e = diag(sqrt(diag(Sigma_e)))*Correlation_matrix*diag(sqrt(diag(Sigma_e))); 
 %if calibrated covariances, set them now to their stored value
-if isfield(estim_params,'calibrated_covariances')
+if isfield(estim_params_,'calibrated_covariances')
     Sigma_e(estim_params_.calibrated_covariances.position)=estim_params_.calibrated_covariances.cov_value;
 end
 % update offset
@@ -89,7 +89,7 @@ end
 %diagonal
 H = diag(sqrt(diag(H)))*Correlation_matrix_ME*diag(sqrt(diag(H)));
 %if calibrated covariances, set them now to their stored value
-if isfield(estim_params,'calibrated_covariances_ME')
+if isfield(estim_params_,'calibrated_covariances_ME')
     H(estim_params_.calibrated_covariances_ME.position)=estim_params_.calibrated_covariances_ME.cov_value;
 end
 