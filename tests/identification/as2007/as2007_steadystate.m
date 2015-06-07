0104f91f4b1350be84caae9839a609f1b64e909b
Johannes Pfeifer <jpfeifer@gmx.de>
2015-06-07 12:18:24 +0200

    Replace initval blocks by steady_state_model blocks in fs2000 unit tests
    Speeds up estimation

--- tests/identification/as2007/as2007_steadystate.m
+++ tests/identification/as2007/as2007_steadystate.m
@@ -1,6 +1,6 @@
-function [ys,check1]=as2007_steadystate(junk, ys)
+function [ys,check1]=as2007_steadystate(ys,exo)
 
-global M_ options_
+global M_
 
 for j=1:size(M_.param_names,1)
   eval([deblank(M_.param_names(j,:)),' = M_.params(j);'])
@@ -21,12 +21,14 @@ YGR=gam_steady;
 INFL = pi_steady;
 INT = pi_steady+rr_steady+4*gam_steady;
 
-ys=[
-pie 
-y 
-R 
-g 
-z 
-YGR 
-INFL
-INT];
+%% end own model equations
+
+for iter = 1:length(M_.params) %update parameters set in the file
+  eval([ 'M_.params(' num2str(iter) ') = ' M_.param_names(iter,:) ';' ])
+end
+
+NumberOfEndogenousVariables = M_.orig_endo_nbr; %auxiliary variables are set automatically
+for ii = 1:NumberOfEndogenousVariables
+  varname = deblank(M_.endo_names(ii,:));
+  eval(['ys(' int2str(ii) ') = ' varname ';']);
+end
