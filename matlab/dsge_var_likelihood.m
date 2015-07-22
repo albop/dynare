0fd76e0c6f6479310f48844cb584868ddfc46382
Johannes Pfeifer <jpfeifer@gmx.de>
2015-07-22 15:11:39 +0200

    Assigning unique function for each input argument of lyapunov_symm.m
    The old way of redefining input arguments to satisfy different options was too error-prone. See 69daaa0460b0ddee97292c39d40355201e316622 of tholden

--- matlab/dsge_var_likelihood.m
+++ matlab/dsge_var_likelihood.m
@@ -162,7 +162,7 @@ end
 %------------------------------------------------------------------------------
 
 % Compute the theoretical second order moments
-tmp0 = lyapunov_symm(T,R*Q*R',DynareOptions.qz_criterium,DynareOptions.lyapunov_complex_threshold, [], [], DynareOptions.debug);
+tmp0 = lyapunov_symm(T,R*Q*R',DynareOptions.lyapunov_fixed_point_tol,DynareOptions.qz_criterium,DynareOptions.lyapunov_complex_threshold, [], [], DynareOptions.debug);
 mf  = BayesInfo.mf1;
 
 % Get the non centered second order moments
