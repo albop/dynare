4202dff6c9c3edd2afa16235d2dedcfdfc3beec5
Johannes Pfeifer <jpfeifer@gmx.de>
2015-07-23 18:13:15 +0200

    marginal_density.m: only compute determinant once
    Uses log(det(SIGMA*increase))=log(increase^npar*det(SIGMA))=npar*log(increase)+log(det(SIGMA))

--- matlab/marginal_density.m
+++ matlab/marginal_density.m
@@ -65,7 +65,7 @@ fprintf(' Done!\n');
 save([M_.fname '_mean.mat'],'xparam1','hh','SIGMA');
 
 fprintf('Estimation::marginal density: I''m computing the posterior log marginal density (modified harmonic mean)... ');
-detSIGMA = det(SIGMA);
+logdetSIGMA = log(det(SIGMA));
 invSIGMA = inv(SIGMA);
 marginal = zeros(9,2);
 linee = 0;
@@ -83,7 +83,7 @@ while check_coverage
                 for i = ifil:EndOfFile
                     deviation  = ((x2(i,:)-MU)*invSIGMA*(x2(i,:)-MU)')/increase;
                     if deviation <= critval
-                        lftheta = -log(p)-(npar*log(2*pi)+log(detSIGMA)+deviation)/2;
+                        lftheta = -log(p)-(npar*log(2*pi)+(npar*log(increase)+logdetSIGMA)+deviation)/2;
                         tmp = tmp + exp(lftheta - logpo2(i) + lpost_mode);
                     end
                 end
@@ -102,12 +102,10 @@ while check_coverage
             disp('Estimation::marginal density: The support of the weighting density function is not large enough...')
             disp('Estimation::marginal density: I increase the variance of this distribution.')
             increase = 1.2*increase;
-            detSIGMA = det(SIGMA*increase);
             linee    = 0;   
         else
             disp('Estimation::marginal density: Let me try again.')
             increase = 1.2*increase;
-            detSIGMA = det(SIGMA*increase);
             linee    = 0;
             if increase > 20
                 check_coverage = 0;
