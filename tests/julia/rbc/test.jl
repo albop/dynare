96869fbd76153da9e55dac8acbbcb973775bb230
Houtan Bastani <houtan@dynare.org>
2015-08-17 11:45:24 +0200

    add workspace() command to the beginning of test.jl

--- tests/julia/rbc/test.jl
+++ tests/julia/rbc/test.jl
@@ -1,3 +1,6 @@
+#clear workspace
+workspace()
+
 # Modification of the path (for packages). Should be done in ~/.juliarc.jl with a fixed path instead.
 unshift!(LOAD_PATH, abspath("../../../julia"))
 
