albop/dynare

commit f143b54142ae1c1f27a22842c3d994323e59f16d

preprocessor: read config file before macroprocessor run. #1039

preprocessor/DynareMain.cc
@@ -30,15 +30,17 @@
 
 #include <unistd.h>
 #include "ExtendedPreprocessorTypes.hh"
+#include "ConfigFile.hh"
 
 /* Prototype for second part of main function
    Splitting main() in two parts was necessary because ParsingDriver.h and MacroDriver.h can't be
    included simultaneously (because of Bison limitations).
 */
-void main2(stringstream &in, string &basename, bool debug, bool clear_all, bool clear_global, bool no_tmp_terms, bool no_log, bool no_warn,
-           bool warn_uninit, bool console, bool nograph, bool nointeractive,
-           bool parallel, const string &parallel_config_file, const string &cluster_name, bool parallel_slave_open_mode,
-           bool parallel_test, bool nostrict, bool check_model_changes, bool minimal_workspace, FileOutputType output_mode, LanguageOutputType lang
+void main2(stringstream &in, string &basename, bool debug, bool clear_all, bool clear_global,
+           bool no_tmp_terms, bool no_log, bool no_warn, bool warn_uninit, bool console,
+           bool nograph, bool nointeractive, bool parallel, ConfigFile &config_file,
+           WarningConsolidation &warnings_arg, bool nostrict, bool check_model_changes,
+           bool minimal_workspace, FileOutputType output_mode, LanguageOutputType lang
 #if defined(_WIN32) || defined(__CYGWIN32__)
            , bool cygwin, bool msvc
 #endif
@@ -278,6 +280,14 @@ main(int argc, char **argv)
   if (pos != string::npos)
     basename.erase(pos);
 
+  WarningConsolidation warnings(no_warn);
+
+  // Process config file
+  ConfigFile config_file(parallel, parallel_test, parallel_slave_open_mode, cluster_name);
+  config_file.getConfigFileInfo(parallel_config_file);
+  config_file.checkPass(warnings);
+  config_file.transformPass();
+
   // Do macro processing
   MacroDriver m;
 
@@ -301,8 +311,9 @@ main(int argc, char **argv)
     return EXIT_SUCCESS;
 
   // Do the rest
-  main2(macro_output, basename, debug, clear_all, clear_global, no_tmp_terms, no_log, no_warn, warn_uninit, console, nograph, nointeractive,
-        parallel, parallel_config_file, cluster_name, parallel_slave_open_mode, parallel_test, nostrict, check_model_changes, minimal_workspace,
+  main2(macro_output, basename, debug, clear_all, clear_global,
+        no_tmp_terms, no_log, no_warn, warn_uninit, console, nograph, nointeractive,
+        parallel, config_file, warnings, nostrict, check_model_changes, minimal_workspace,
         output_mode, language
 #if defined(_WIN32) || defined(__CYGWIN32__)
         , cygwin, msvc

preprocessor/DynareMain2.cc
@@ -25,30 +25,26 @@
 #include "ExtendedPreprocessorTypes.hh"
 
 void
-main2(stringstream &in, string &basename, bool debug, bool clear_all, bool clear_global, bool no_tmp_terms, bool no_log, bool no_warn, bool warn_uninit, bool console, bool nograph, bool nointeractive,
-      bool parallel, const string &parallel_config_file, const string &cluster_name, bool parallel_slave_open_mode,
-      bool parallel_test, bool nostrict, bool check_model_changes, bool minimal_workspace, FileOutputType output_mode, LanguageOutputType language
+main2(stringstream &in, string &basename, bool debug, bool clear_all, bool clear_global,
+      bool no_tmp_terms, bool no_log, bool no_warn, bool warn_uninit, bool console,
+      bool nograph, bool nointeractive, bool parallel, ConfigFile &config_file,
+      WarningConsolidation &warnings, bool nostrict, bool check_model_changes,
+      bool minimal_workspace, FileOutputType output_mode, LanguageOutputType language
 #if defined(_WIN32) || defined(__CYGWIN32__)
       , bool cygwin, bool msvc
 #endif
       )
 {
-  WarningConsolidation warnings(no_warn);
-
   ParsingDriver p(warnings, nostrict);
 
   // Do parsing and construct internal representation of mod file
   ModFile *mod_file = p.parse(in, debug);
-  ConfigFile config_file(parallel, parallel_test, parallel_slave_open_mode, cluster_name);
-  config_file.getConfigFileInfo(parallel_config_file);
 
   // Run checking pass
   mod_file->checkPass();
-  config_file.checkPass(warnings);
 
   // Perform transformations on the model (creation of auxiliary vars and equations)
   mod_file->transformPass(nostrict);
-  config_file.transformPass();
 
   // Evaluate parameters initialization, initval, endval and pounds
   mod_file->evalAllExpressions(warn_uninit);

commit 1dd4e03770fc296f2062bcfd18353d01fe2430ce

preprocessor: fix build on wheezy due to bug introduced in f143b54142ae1c1f27a22842c3d994323e59f16d

preprocessor/DynareMain.cc
@@ -26,9 +26,9 @@
 #ifndef PACKAGE_VERSION
 # define PACKAGE_VERSION 4.
 #endif
-#include "macro/MacroDriver.hh"
 
 #include <unistd.h>
+#include "ParsingDriver.hh"
 #include "ExtendedPreprocessorTypes.hh"
 #include "ConfigFile.hh"
 
@@ -45,6 +45,10 @@ void main2(stringstream &in, string &basename, bool debug, bool clear_all, bool 
            , bool cygwin, bool msvc
 #endif
            );
+
+void main1(char *modfile, string &basename, bool debug, bool save_macro, string &save_macro_file,
+           bool no_line_macro,
+           map<string, string> &defines, vector<string> &path, stringstream &macro_output);
 
 void
 usage()
@@ -296,23 +300,8 @@ main(int argc, char **argv)
     path.push_back(*it);
 
   // Do macro processing
-  MacroDriver m;
-
   stringstream macro_output;
-  m.parse(argv[1], macro_output, debug, no_line_macro, defines, path);
-  if (save_macro)
-    {
-      if (save_macro_file.empty())
-        save_macro_file = basename + "-macroexp.mod";
-      ofstream macro_output_file(save_macro_file.c_str());
-      if (macro_output_file.fail())
-        {
-          cerr << "Cannot open " << save_macro_file << " for macro output" << endl;
-          exit(EXIT_FAILURE);
-        }
-      macro_output_file << macro_output.str();
-      macro_output_file.close();
-    }
+  main1(argv[1], basename, debug, save_macro, save_macro_file, no_line_macro, defines, path, macro_output);
 
   if (only_macro)
     return EXIT_SUCCESS;

preprocessor/DynareMain1.cc
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2015 Dynare Team
+ *
+ * This file is part of Dynare.
+ *
+ * Dynare is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Dynare is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <sstream>
+#include <fstream>
+
+#include "macro/MacroDriver.hh"
+
+void
+main1(char *modfile, string &basename, bool debug, bool save_macro, string &save_macro_file, bool no_line_macro,
+      map<string, string> &defines, vector<string> &path, stringstream &macro_output)
+{
+  // Do macro processing
+  MacroDriver m;
+
+  m.parse(modfile, macro_output, debug, no_line_macro, defines, path);
+  if (save_macro)
+    {
+      if (save_macro_file.empty())
+        save_macro_file = basename + "-macroexp.mod";
+      ofstream macro_output_file(save_macro_file.c_str());
+      if (macro_output_file.fail())
+        {
+          cerr << "Cannot open " << save_macro_file << " for macro output" << endl;
+          exit(EXIT_FAILURE);
+        }
+      macro_output_file << macro_output.str();
+      macro_output_file.close();
+    }
+}

preprocessor/Makefile.am
@@ -45,6 +45,7 @@ dynare_m_SOURCES = \
 	MinimumFeedbackSet.cc \
 	MinimumFeedbackSet.hh \
 	DynareMain.cc \
+	DynareMain1.cc \
 	DynareMain2.cc \
 	CodeInterpreter.hh \
 	ExternalFunctionsTable.cc \