818fa473756327395f2a853e9404ba550ea25543
Michel Juillard <michel.juillard@mjui.fr>
2015-10-09 14:21:50 +0200

    Revert "adding new functions for penalty optimization"
    This reverts commit b9e535e99e25bf53483fc7fbdaf60fb1aec3eb02.

--- matlab/optimization/penalty_objective_function.m
+++
@@ -1,6 +0,0 @@
-function [fval,DLIK,Hess,exit_flag] = objective_function_penalty(x0,fcn,penalty,varargin)
-    [fval,DLIK,Hess,exit_flag,SteadyState,trend_coeff,info] = fcn(x0,varargin{:});
-    if info(1) ~= 0
-        fval = penalty + info(2);
-    end
-end
