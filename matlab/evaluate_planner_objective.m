660fc64471fb4e3bda5bd1689180522485ee41cc
Michel Juillard <michel.juillard@mjui.fr>
2015-11-08 16:51:06 +0100

    adding check for model size before computing value of planner objective.

--- matlab/evaluate_planner_objective.m
+++ matlab/evaluate_planner_objective.m
@@ -32,6 +32,12 @@ dr = oo.dr;
 exo_nbr = M.exo_nbr;
 nstatic = M.nstatic;
 nspred = M.nspred;
+if nspred > 180
+    disp(' ')
+    disp(['evaluate_planner_objective: model too large, can''t evaluate planner ' ...
+          'objective'])
+    return NaN
+end
 beta = get_optimal_policy_discount_factor(M.params,M.param_names);
     
 Gy = dr.ghx(nstatic+(1:nspred),:);
