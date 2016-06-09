335a188dacba7e4aa48e3a479cd83e2b303255da
Stéphane Adjemian (Hermes) <stephane.adjemian@univ-lemans.fr>
2016-06-09 10:32:25 +0200

    Fixed bug in extended path.
    Allow innovations with zero variance (ie do not force removal of the
    variables from the varexo list).

--- matlab/ep/extended_path_shocks.m
+++ matlab/ep/extended_path_shocks.m
@@ -16,13 +16,13 @@ function [shocks, spfm_exo_simul, innovations, DynareResults] = extended_path_sh
 %
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
-    
+
 % Simulate shocks.
 if isempty(exogenousvariables)
     switch ep.innovation_distribution
       case 'gaussian'
-        shocks = transpose(transpose(innovations.covariance_matrix_upper_cholesky)*randn(innovations.effective_number_of_shocks,sample_size));
-        shocks(:,innovations.positive_var_indx) = shocks;
+        shocks = zeros(sample_size, DynareModel.exo_nbr);
+        shocks(:,innovations.positive_var_indx) = transpose(transpose(innovations.covariance_matrix_upper_cholesky)*randn(innovations.effective_number_of_shocks,sample_size));
       case 'calibrated'
         options = DynareOptions;
         options.periods = options.ep.periods;
