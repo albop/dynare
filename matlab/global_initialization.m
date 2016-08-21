albop/dynare

commit 75d699cbfa7057b3543ae5fbc0117fea799ac1e9

fixing dates in shocks_decomposition. Interface still missing

matlab/graph_decomp.m
@@ -35,7 +35,7 @@ comp_nbr = size(z,2)-1;
 
 gend = size(z,3);
 freq = initial_date.freq;
-initial_period = initial_date.period + initial_date.subperiod/freq;
+initial_period = initial_date.time(1) + initial_date.time(2)/freq;
 x = initial_period-1/freq:(1/freq):initial_period+(gend-1)/freq;
 
 nvar = length(i_var);

commit 0ebef17df238ff1872b0c8642f4c7f24bad20575

fix shocks_decomposition bug introduced in commit 75d699cbfa7057b3543ae5fbc0117fea799ac1e9

matlab/global_initialization.m
@@ -678,9 +678,7 @@ options_.dr_logarithmic_reduction_tol = 1e-12;
 options_.dr_logarithmic_reduction_maxiter = 100;
 
 % dates for historical time series
-options_.initial_date.freq = 1;
-options_.initial_date.period = 1;
-options_.initial_date.subperiod = 0;
+options_.initial_date = dates();
 
 % discretionary policy
 options_.discretionary_policy = 0;

matlab/graph_decomp.m
@@ -34,9 +34,13 @@ new_colormap = DynareOptions.colormap;
 comp_nbr = size(z,2)-1;
 
 gend = size(z,3);
-freq = initial_date.freq;
-initial_period = initial_date.time(1) + initial_date.time(2)/freq;
-x = initial_period-1/freq:(1/freq):initial_period+(gend-1)/freq;
+if isempty(initial_date)
+    x = 0:gend;
+else
+    freq = initial_date.freq;
+    initial_period = initial_date.time(1) + initial_date.time(2)/freq;
+    x = initial_period-1/freq:(1/freq):initial_period+(gend-1)/freq;
+end
 
 nvar = length(i_var);
 