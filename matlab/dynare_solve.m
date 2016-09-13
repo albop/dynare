albop/dynare

commit 80049a6d0944f65b1a255ad884a62ec0b4313ea2

dynare_solve.m: make sure str2func is only used on strings
Otherwise, code crashes

matlab/dynare_solve.m
@@ -102,7 +102,11 @@ if options.solve_algo == 0
         [x,fval,exitval,output] = fsolve(func,x,options4fsolve,varargin{:});
     else
         % Under Octave, use a wrapper, since fsolve() does not have a 4th arg
-        func2 = str2func(func);
+        if isstring(func)
+            func2 = str2func(func);
+        else
+            func2 = func;
+        end
         func = @(x) func2(x, varargin{:});
         % The Octave version of fsolve does not converge when it starts from the solution
         fvec = feval(func,x);
@@ -116,7 +120,11 @@ if options.solve_algo == 0
     if exitval == 1
         info = 0;
     elseif exitval > 1
-        func2 = str2func(func);
+        if isstring(func)
+            func2 = str2func(func);
+        else
+            func2 = func;
+        end
         func = @(x) func2(x, varargin{:});
         fvec = feval(func,x);
         if max(abs(fvec)) >= tolf

commit 919647477ecb01579dda7cd107c933e050b018da

replace isstring with ischar as isstring is not supported in Octave. Bug introduced in 80049a6d0944f65b1a255ad884a62ec0b4313ea2

matlab/dynare_solve.m
@@ -17,7 +17,7 @@ function [x,info,fvec,fjac] = dynare_solve(func,x,options,varargin)
 % SPECIAL REQUIREMENTS
 %    none
 
-% Copyright (C) 2001-2015 Dynare Team
+% Copyright (C) 2001-2016 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -103,7 +103,7 @@ if options.solve_algo == 0
         [x,fval,exitval,output] = fsolve(func,x,options4fsolve,varargin{:});
     else
         % Under Octave, use a wrapper, since fsolve() does not have a 4th arg
-        if isstring(func)
+        if ischar(func)
             func2 = str2func(func);
         else
             func2 = func;
@@ -121,7 +121,7 @@ if options.solve_algo == 0
     if exitval == 1
         info = 0;
     elseif exitval > 1
-        if isstring(func)
+        if ischar(func)
             func2 = str2func(func);
         else
             func2 = func;