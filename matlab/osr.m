e09b4571d73f35214bab6c71decfe23c6c281028
Houtan Bastani <houtan@dynare.org>
2016-09-23 16:12:40 +0200

    osr: fix call to strmatch for octave. Closes #1291

--- matlab/osr.m
+++ matlab/osr.m
@@ -51,7 +51,8 @@ oo_=make_ex_(M_,options_,oo_);
 np = size(params,1);
 i_params = zeros(np,1);
 for i=1:np
-    i_params(i) = strmatch(deblank(params(i,:)),M_.param_names,'exact');
+    str = deblank(params(i,:));
+    i_params(i) = strmatch(str{:}, M_.param_names, 'exact');
 end
 
 skipline()
