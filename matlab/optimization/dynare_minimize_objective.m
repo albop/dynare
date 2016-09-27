albop/dynare

commit 1c892bb3e99a0ac928a304b9784506dc776c59d4

octave:  try to load the needed package before ending with an error

matlab/optimization/dynare_minimize_objective.m
@@ -135,9 +135,17 @@ switch minimizer_algorithm
         simulated_annealing(objective_function,start_par_value,sa_options,LB,UB,varargin{:});
   case 3
     if isoctave && ~user_has_octave_forge_package('optim')
-        error('Optimization algorithm 3 requires the optim package')
+        try
+            pkg load optim
+        catch
+            error('Optimization algorithm 3 requires the optim package')
+        end
     elseif ~isoctave && ~user_has_matlab_license('optimization_toolbox')
-        error('Optimization algorithm 3 requires the Optimization Toolbox')
+        try
+            pkg load optimization_toolbox
+        catch
+            error('Optimization algorithm 3 requires the Optimization Toolbox')
+        end
     end
     % Set default optimization options for fminunc.
     optim_options = optimset('display','iter','MaxFunEvals',100000,'TolFun',1e-8,'TolX',1e-6);

commit 744b52d362c5823e445dd31c772c7899609bb061

fix bug introduced in 1c892bb3e99a0ac928a304b9784506dc776c59d4

matlab/optimization/dynare_minimize_objective.m
@@ -141,11 +141,7 @@ switch minimizer_algorithm
             error('Optimization algorithm 3 requires the optim package')
         end
     elseif ~isoctave && ~user_has_matlab_license('optimization_toolbox')
-        try
-            pkg load optimization_toolbox
-        catch
-            error('Optimization algorithm 3 requires the Optimization Toolbox')
-        end
+        error('Optimization algorithm 3 requires the Optimization Toolbox')
     end
     % Set default optimization options for fminunc.
     optim_options = optimset('display','iter','MaxFunEvals',100000,'TolFun',1e-8,'TolX',1e-6);